09caf2949e81b8359e0d50edc6e0ea3f34449745
Ryan Kienstra <kienstraryan@gmail.com>
2022-02-10 20:03:03 -0600

    Do npm run lint:js:fix

--- wp-modules/app/includes/js/src/visual/PatternPicker.js
+++ wp-modules/app/includes/js/src/visual/PatternPicker.js
@@ -93,7 +93,7 @@ export function PatternPicker({ patterns: allPatterns, selectMultiple }) {
 								aria-checked={isChecked}
 								className={
 									isChecked
-										? 'min-h-[300px] bg-gray-200 border-2 border-solid border-sky-500'
+										? 'min-h-[300px] border-2 border-solid border-sky-500 bg-gray-200'
 										: 'min-h-[300px] bg-gray-200'
 								}
 								onClick={() =>
