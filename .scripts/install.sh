701355a60a27f5b8709cecbffd50e1c0bcbe62e5
Phil Johnston <phil.johnston@wpengine.com>
2022-02-16 13:58:12 -0500

    Circle tweak

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -1,5 +1,14 @@
 # Do not touch this file.
 
+# If it doesn't already exist, clone wp-plugin-sidekick to the plugins directory.
+cd ../;
+FILE=wp-plugin-sidekick/
+if [ ! -d "$FILE" ]; then
+	git clone https://github.com/johnstonphilip/wp-plugin-sidekick;
+fi
+
+cd -;
+
 # Loop through each wp-module in the plugin.
 for DIR in wp-modules/*/; do
 	# Go to the directory of this custom-module.
