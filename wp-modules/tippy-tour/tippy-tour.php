cb5dc2efd0efd38215cd9640c91c006d51c951c0
Phil Johnston <phil.johnston@wpengine.com>
2022-03-10 16:10:40 -0500

    Tippy Tour is now just PHP

--- wp-modules/tippy-tour/tippy-tour.php
+++ wp-modules/tippy-tour/tippy-tour.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Module Name: Tippy Tour
+ * Module Tippy Tour
  * Description: This module contains the code for the tippy tour
  * Namespace: TippyTour
  *
@@ -16,4 +16,34 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-require 'includes/php/enqueue-scripts.php';
+// Get the tour data.
+$tour_data = require 'tour-data.php';
+
+// Do nothing if the slug of the tour is not a URL param.
+if ( ! isset( $_GET[ $tour_data['tourSlug'] ] ) ) {
+	return;
+}
+
+/**
+ * Enqueue the stepped tour file in the WpeBeacon plugin.
+ */
+function enqueue_stepped_tour_file() {
+	if ( function_exists( 'Tippy\SteppedTour\enqueue' ) ) {
+		add_action( 'init', 'Tippy\SteppedTour\enqueue' );
+	}
+}
+add_action( 'init', __NAMESPACE__ . '\enqueue_stepped_tour_file', 9 );
+
+/**
+ * Output the javascript that creates this tour.
+ */
+function create_tour() {
+	if ( function_exists( 'Tippy\SteppedTour\enqueue' ) ) {
+		wp_add_inline_script(
+			'wpe_beacon_stepped_tour',
+			'wpeBeaconCreateSteppedTour(' . wp_json_encode( require 'tour-data.php' ) . ');'
+		);
+	}
+}
+add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\create_tour' );
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\create_tour' );
---
+++ wp-modules/tippy-tour/tour-data.php
@@ -0,0 +1,118 @@
+<?php
+return [
+	'tourTitle'   => __( 'FSE Studio Tour', 'fse-studio' ),
+	'tourSlug'    => 'FseStudioTour',
+	'stepsInTour' => [
+		[
+			'initialData' => [
+				'html' => __( 'Welcome to the Fse Studio Tour. Let\'s make a theme.', 'fse-studio' ),
+			],
+			'navigation'  => [
+				'backButton' => false,
+				'nextButton' => true,
+			],
+		],
+		[
+			'highlightedElement' => [
+				'role' => 'button',
+				'name' => __( 'Create a new theme', 'fse-studio' ),
+			],
+			'beaconPosition'     => 'auto',
+			'initialData'        => [
+				'html' => __( 'Click to create a new theme', 'fse-studio' ),
+			],
+			'completionData'     => [
+				'html'    => __( 'Great Job', 'fse-studio' ),
+				'type'    => 'elementHasValue',
+				'element' => [
+					'role'  => 'textbox',
+					'name'  => __( 'Theme Name', 'fse-studio' ),
+					'value' => __( 'My New Theme', 'fse-studio' ),
+				],
+			],
+			'navigation'         => [
+				'backButton' => false,
+				'nextButton' => false,
+				'onComplete' => 'goToNextStep',
+			],
+		],
+		[
+			'highlightedElement' => [
+				'role' => 'textbox',
+				'name' => __( 'Theme Name', 'fse-studio' ),
+			],
+			'beaconPosition'     => 'right',
+			'initialData'        => [
+				'html' => __( 'Give your theme a unique name', 'fse-studio' ),
+			],
+			'completionData'     => [
+				'html'    => __( 'Give your theme a unique name', 'fse-studio' ) . __( 'Looks good! Feel free to keep working on your unique name. When ready, click "next".', 'fse-studio' ),
+				'type'    => 'elementDoesNotHaveValues',
+				'element' => [
+					'role'   => 'textbox',
+					'name'   => __( 'Theme Name', 'fse-studio' ),
+					'values' => [
+						'',
+						__( 'My New Theme', 'fse-studio' ),
+					],
+				],
+			],
+			'navigation'         => [
+				'backButton' => false,
+				'nextButton' => true,
+			],
+		],
+		[
+			'highlightedElement' => [
+				'role' => 'textbox',
+				'name' => __( 'Directory Name', 'fse-studio' ),
+			],
+			'beaconPosition'     => 'right',
+			'initialData'        => [
+				'html' => __( 'Give your theme a unique directory name.', 'fse-studio' ) . __( 'Ideally this matches your theme\'s name, but without any special characters or spaces. Replace spaces with dashes.', 'fse-studio' ),
+			],
+			'completionData'     => [
+				'html'    => __( 'Give your theme a unique directory name.', 'fse-studio' ) . __( 'Ideally this matches your theme\'s name, but without any special characters or spaces. Replace spaces with dashes.', 'fse-studio' ),
+				'type'    => 'elementDoesNotHaveValues',
+				'element' => [
+					'role'   => 'textbox',
+					'name'   => __( 'Directory Name', 'fse-studio' ),
+					'values' => [
+						'',
+						'my-new-theme',
+					],
+				],
+			],
+			'navigation'         => [
+				'backButton' => true,
+				'nextButton' => true,
+			],
+		],
+		[
+			'highlightedElement' => [
+				'role' => 'textbox',
+				'name' => __( 'Namespace', 'fse-studio' ),
+			],
+			'beaconPosition'     => 'right',
+			'initialData'        => [
+				'html' => __( 'Give your theme a unique namespace', 'fse-studio' ) . __( 'This is the PHP namespace that will be used to ensure your theme\'s code does not conflict with any other themes.', 'fse-studio' ),
+			],
+			'completionData'     => [
+				'html'    => __( 'Great Job', 'fse-studio' ),
+				'type'    => 'elementDoesNotHaveValues',
+				'element' => [
+					'role'   => 'textbox',
+					'name'   => __( 'Namespace', 'fse-studio' ),
+					'values' => [
+						'',
+						'MyNewTheme',
+					],
+				],
+			],
+			'navigation'         => [
+				'backButton' => true,
+				'nextButton' => true,
+			],
+		],
+	],
+];
