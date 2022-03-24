562ae9f8770c84e5424e0f8a718add52d34d8531
Ryan Kienstra <kienstraryan@gmail.com>
2022-03-24 10:48:50 -0700

    Add a helper function to convert camelCase to Upper Case

---
+++ wp-modules/app/js/src/utils/convertToUpperCase.js
@@ -0,0 +1,17 @@
+/** @param {string} name */
+function capitalize( name ) {
+	return name.charAt( 0 ).toUpperCase() + name.slice( 1 );
+}
+
+/**
+ * Converts a string to a Upper Case.
+ *
+ * @param {string?} toConvert The string to convert, like 'exampleHere'.
+ * @return {string} The string in Upper Case, like 'Example Here'.
+ */
+export default function convertToUpperCase( toConvert = '' ) {
+	return capitalize( toConvert.replace(
+		/(?<=[a-z1-9])[A-Z1-9](?=[a-z]+)/g,
+		( match ) => ` ${ capitalize( match ) }`
+	) );
+}
