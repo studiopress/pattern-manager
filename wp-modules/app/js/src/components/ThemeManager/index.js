7fd9e578045a30f80a969d67bb8aea4328b9816b
marksabbath <marksabbath@gmail.com>
2022-03-30 10:13:19 -0300

    Fix lint

--- wp-modules/app/js/src/components/ThemeManager/index.js
+++ wp-modules/app/js/src/components/ThemeManager/index.js
@@ -254,7 +254,12 @@ function ThemeDataEditor() {
 				<button
 					type="button"
 					className="inline-flex items-center px-4 py-2 border border-4 border-transparent text-sm font-medium rounded-sm shadow-sm text-white bg-wp-gray hover:bg-[#4c5a60] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-wp-blue"
-					style={ ! currentTheme.existsOnDisk && { display: 'none' } || {} }
+					style={
+						( ! currentTheme.existsOnDisk && {
+							display: 'none',
+						} ) ||
+						{}
+					}
 					onClick={ () => {
 						currentTheme.export().then( ( response ) => {
 							window.open( response );
