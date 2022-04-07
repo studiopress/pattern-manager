5bbac1fffb732c7c3b72fc4a1f514aab156f857a
Mike McAlister <m@mikemcalister.com>
2022-04-07 14:21:55 -0500

    Style improvements to gradient component

--- wp-modules/app/js/src/components/ThemeJsonEditor/GradientEditor.js
+++ wp-modules/app/js/src/components/ThemeJsonEditor/GradientEditor.js
@@ -13,35 +13,38 @@ export default function GradientEditor({properties, schemaPosition}) {
 	const slugValue = currentThemeJsonFile.getValue( 'settings', schemaPosition + ',slug', properties.slug.type );
 	const gradientValue = currentThemeJsonFile.getValue( 'settings', schemaPosition + ',gradient', properties.gradient.type );
 
-	return <div>
-		<div className="name">
-			<div>{ __( 'Name', 'fse-studio' ) }</div>
-			<div>{ properties.name.description }</div>
-			<input type="text" value={nameValue} onChange={(event) => {
-				currentThemeJsonFile.setValue( 'settings', schemaPosition + ',name', event.target.value );
-			}} />
-		</div>
-		<div className="slug">
-			<div>{ __( 'Slug', 'fse-studio' ) }</div>
-			<div>{ properties.slug.description }</div>
-			<input type="text" value={slugValue} onChange={(event) => {
-				currentThemeJsonFile.setValue( 'settings', schemaPosition + ',slug', event.target.value );
-			}} />
-		</div>
-		<div className="gradient">
-			<div>{ __( 'Name', 'fse-studio' ) }</div>
-			<div>{ properties.gradient.description }</div>
-			<div>
-				<CustomGradientPicker
-					value={ gradientValue }
-					onChange={ (newValue) => {
-						currentThemeJsonFile.setValue( 'settings', schemaPosition + ',gradient', newValue );
-					}}
-				/>
+	return <div className="bg-gray-100 p-5 rounded">
+		<div className="flex justify-between items-center gap-5">
+			<div className="flex flex-col gap-5">
+				<div className="name flex flex-col space-y-1">
+					<div className="font-semibold">{ __( 'Name', 'fse-studio' ) }</div>
+					<div>{ properties.name.description }</div>
+					<input type="text" value={nameValue} onChange={(event) => {
+						currentThemeJsonFile.setValue( 'settings', schemaPosition + ',name', event.target.value );
+					}} />
+				</div>
+				<div className="slug flex flex-col space-y-1">
+					<div className="font-semibold">{ __( 'Slug', 'fse-studio' ) }</div>
+					<div>{ properties.slug.description }</div>
+					<input type="text" value={slugValue} onChange={(event) => {
+						currentThemeJsonFile.setValue( 'settings', schemaPosition + ',slug', event.target.value );
+					}} />
+				</div>
+			</div>
+			<div className="gradient mt-12">
+				<div>
+					<CustomGradientPicker
+						value={ gradientValue }
+						onChange={ (newValue) => {
+							currentThemeJsonFile.setValue( 'settings', schemaPosition + ',gradient', newValue );
+						}}
+					/>
+				</div>
 			</div>
 		</div>
-		<div>
+		<div className="text-right border-t border-dotted border-gray-300 pt-3 mt-5">
 			<button
+				className="text-red-500 hover:text-red-700"
 				onClick={() => {
 					currentThemeJsonFile.setValue( 'settings', schemaPosition, false );
 				}}
