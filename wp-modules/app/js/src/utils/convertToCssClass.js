7c2635622db601944ca3d28a0860143678fb4c6d
Ryan Kienstra <kienstraryan@gmail.com>
2022-04-13 10:37:11 -0500

    Fix types for optional arguments

--- wp-modules/app/js/src/utils/convertToCssClass.js
+++ wp-modules/app/js/src/utils/convertToCssClass.js
@@ -1,7 +1,7 @@
 /**
  * Converts a string to a css class.
  *
- * @param {string?} toConvert The string to convert to a css class like 'ExampleHere'.
+ * @param {string} [toConvert] The string to convert to a css class like 'ExampleHere'.
  * @return {string} The string as a slug, like 'example-here'.
  */
 export default function convertToCssClass( toConvert = '' ) {
--- wp-modules/app/js/src/utils/convertToSlug.js
+++ wp-modules/app/js/src/utils/convertToSlug.js
@@ -1,7 +1,7 @@
 /**
  * Converts a string to a slug.
  *
- * @param {string?} toConvert The string to convert to a slug like 'Example Here'.
+ * @param {string} [toConvert] The string to convert to a slug like 'Example Here'.
  * @return {string} The string as a slug, like 'example-here'.
  */
 export default function convertToSlug( toConvert = '' ) {
