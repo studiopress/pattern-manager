9c55a3d98e05181c829bcbb1581d9f8fd3268b9b
marksabbath <marksabbath@gmail.com>
2022-04-14 13:39:07 -0300

    Set boolean when no type is available

--- wp-modules/app/js/src/components/ThemeJsonEditor/index.js
+++ wp-modules/app/js/src/components/ThemeJsonEditor/index.js
@@ -340,7 +340,7 @@ function RenderProperties( { isVisible, properties, schemaPosition, topLevelSett
 
 	for( const propertyName in properties ) {
 		renderedProperties.push(
-			<div key={propertyName} hidden={!isVisible} className={`fses-${convertToCssClass(propertyName)} fses-type-${properties[propertyName].type}`}>
+			<div key={propertyName} hidden={!isVisible} className={`fses-${convertToCssClass(propertyName)} fses-type-${convertToCssClass(properties[propertyName].type) || "boolean" }`}>
 				<div className="sm:grid sm:grid-cols-4 sm:gap-4 py-6 sm:items-top">
 					<label
 						htmlFor={propertyName}
