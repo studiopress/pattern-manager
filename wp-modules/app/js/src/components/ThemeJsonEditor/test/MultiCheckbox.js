3920c95d5c42915244a0a3728923a7d4fbe47984
Ryan Kienstra <kienstraryan@gmail.com>
2022-04-19 13:46:16 -0500

    Fix linting errors

--- wp-modules/app/js/src/components/ThemeJsonEditor/test/MultiCheckbox.js
+++ wp-modules/app/js/src/components/ThemeJsonEditor/test/MultiCheckbox.js
@@ -7,26 +7,25 @@ import MultiCheckbox from '../MultiCheckbox';
 function Container( { initialValue } ) {
 	const [ checkboxValue, setCheckboxValue ] = useState( initialValue );
 
-	return <MultiCheckbox
-		value={ checkboxValue }
-		onChange={ setCheckboxValue }
-	/>;
+	return (
+		<MultiCheckbox value={ checkboxValue } onChange={ setCheckboxValue } />
+	);
 }
 
 describe( 'MultiCheckbox', () => {
 	const units = [ 'px', 'em', 'rem', 'vh', 'vw', '%' ];
 
 	it.each( units )( 'should have checked checkboxes', ( unit ) => {
-        render( <Container initialValue={ units } /> );
+		render( <Container initialValue={ units } /> );
 
 		expect( screen.getByLabelText( unit ).checked ).toEqual( true );
 	} );
 
-    it( 'should handle unchecking', () => {
-        render( <Container initialValue={ units } /> );
+	it( 'should handle unchecking', () => {
+		render( <Container initialValue={ units } /> );
 
-        const checkbox = screen.getByLabelText( 'px' );
-        fireEvent.click( checkbox );
-        expect( checkbox.checked ).toEqual( false );
-    } );
+		const checkbox = screen.getByLabelText( 'px' );
+		fireEvent.click( checkbox );
+		expect( checkbox.checked ).toEqual( false );
+	} );
 } );
