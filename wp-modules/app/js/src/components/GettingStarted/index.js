b7a95bb8972244f4857ce34b872dd0e85e7f4e87
Phil Johnston <phil.johnston@wpengine.com>
2022-06-15 16:21:47 -0400

    Move the create button above the fold

--- wp-modules/app/js/src/components/GettingStarted/index.js
+++ wp-modules/app/js/src/components/GettingStarted/index.js
@@ -11,14 +11,33 @@ export default function GettingStarted() {
 		<>
 			<div className="bg-fses-gray mx-auto p-12 w-full">
 				<div className="max-w-7xl mx-auto">
-					<h1 className="text-4xl mb-3">
-						{ __( 'Welcome to FSE Studio', 'fse-studio' ) }
-					</h1>
-					<p className="text-lg max-w-2xl">
-						Build your full site editing themes faster and easier
-						with FSE Studio! Check out the video below to learn how
-						to get started.
-					</p>
+					<div className="flex">
+						<div className="grow">
+							<h1 className="text-4xl mb-3">
+								{ __( 'Welcome to FSE Studio', 'fse-studio' ) }
+							</h1>
+							<p className="text-lg max-w-2xl">
+								Build your full site editing themes faster and easier
+								with FSE Studio! Check out the video below to learn how
+								to get started.
+							</p>
+						</div>
+						<div className="flex">
+							<button
+								type="button"
+								className="inline-flex self-center items-center px-4 py-2 border-4 border-transparent text-lg font-medium rounded-sm shadow-sm text-white bg-wp-blue hover:bg-wp-blue-hover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-wp-blue"
+								onClick={ () => {
+									createNewTheme( themes, currentThemeId );
+								} }
+							>
+								{ __(
+									'Start Creating Your Theme',
+									'fse-studio'
+								) }{ ' ' }
+								&rarr;
+							</button>
+						</div>
+					</div>
 				</div>
 			</div>
 
