f6d683a3d71592bcaedc3cebcd0817742205dd63
Michael Day <michael.day@wpengine.com>
2022-07-21 13:53:00 -0500

    Add update logic for theme style variations

--- wp-modules/theme-json-data-handlers/theme-json-data-handlers.php
+++ wp-modules/theme-json-data-handlers/theme-json-data-handlers.php
@@ -16,114 +16,35 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-/**
- * Get data for a single themes in the format used by FSE Theme Manager.
- *
- * @param string $id The directory name of the theme in question.
- * @return array
- */
-function get_theme_json_file( $id ) {
-	$theme_json_files = get_all_theme_json_files();
-	return $theme_json_files[ $id ];
-}
+function update_theme_style( $style ) {
 
-/**
- * Get data for all of the installed themes in the format used by FSE Theme Manager.
- *
- * @return array
- */
-function get_all_theme_json_files() {
+	$style_default = array(
+		'id'	=> '',
+		'body' 	=> [],
+	);
+
+	$style = wp_parse_args( $style, $style_default );
 
 	// Spin up the filesystem api.
 	$wp_filesystem = \FseStudio\GetWpFilesystem\get_wp_filesystem_api();
 
-	$module_dir_path = module_dir_path( __FILE__ );
+	$wp_theme_dir = get_template_directory();
 
-	$wp_head_and_wp_footer = get_wp_head_and_wp_footer();
-
-	/**
-	 * Scan theme-json-files directory and get all the default theme.json files.
-	 */
-	$theme_json_file_paths = glob( $module_dir_path . '/theme-json-files/*.json' );
-
-	$theme_json_files = array();
-
-	foreach ( $theme_json_file_paths as $path ) {
-		$theme_json_files[ basename( $path, '.json' ) ] = json_decode( $wp_filesystem->get_contents( $path ), true );
+	if ( ! isset( $style['styles'] ) ) {
+		$styles_dir = $wp_theme_dir . '/styles/';
+		$style_body = json_encode( $style, JSON_PRETTY_PRINT );
+		$file_name  = sanitize_title( $style['id'] ) . '.php';
 	}
 
-	/**
-	 * Scan each theme to get all theme.json files.
-	 */
-	$wp_filesystem = \FseStudio\GetWpFilesystem\get_wp_filesystem_api();
-	$wp_themes_dir = $wp_filesystem->wp_themes_dir();
-
-	$themes = glob( $wp_themes_dir . '*' );
-
-	foreach ( $themes as $theme ) {
-		if ( $wp_filesystem->exists( $theme . '/theme.json' ) ) {
-			$theme_json_files[ basename( $theme ) ] = json_decode( $wp_filesystem->get_contents( $theme . '/theme.json' ), true );
-		}
+	if ( ! $wp_filesystem->exists( $styles_dir ) ) {
+		$wp_filesystem->mkdir( $styles_dir );
 	}
 
-	return $theme_json_files;
-}
-
-/**
- * Run a string of HTML through the_content filters. This makes it so everything needed will be rendered in wp_footer.
- *
- * @param string $content The html content to run through the filters.
- * @return bool
- */
-function do_the_content_things( $content ) {
-
-	// Run through the actions that are typically taken on the_content.
-	$content = do_blocks( $content );
-	$content = wptexturize( $content );
-	$content = convert_smilies( $content );
-	$content = shortcode_unautop( $content );
-	$content = wp_filter_content_tags( $content );
-	$content = do_shortcode( $content );
-
-	// Handle embeds for block template parts.
-	global $wp_embed;
-	$content = $wp_embed->autoembed( $content );
-
-	return $content;
-}
-
-/**
- * Run all pattern HTML through the_content filters, and return wp_head and wp_footer html.
- *
- * @return bool
- */
-function get_wp_head_and_wp_footer() {
-	global $fsestudio_api_get_theme_json_file;
-
-	if ( ! $fsestudio_api_get_theme_json_file ) {
-		return;
-	}
-
-	$all_patterns = \FseStudio\PatternDataHandlers\get_patterns();
-
-	$rendered_patterns = array();
-
-	// Get the PHP rendered version of each block.
-	foreach ( $all_patterns as $pattern ) {
-		$rendered_patterns[ $pattern['name'] ] = do_the_content_things( $pattern['content'] );
-	}
-
-	ob_start();
-	wp_head();
-	$wp_head = ob_get_clean();
-
-	ob_start();
-	wp_footer();
-	$wp_footer = ob_get_clean();
-
-	return array(
-		'wp_head'          => $wp_head,
-		'wp_footer'        => $wp_footer,
-		'renderedPatterns' => $rendered_patterns,
+	$style_file_created = $wp_filesystem->put_contents(
+		$styles_dir . $file_name,
+		$style_body,
+		FS_CHMOD_FILE
 	);
+
+	return $style_file_created;
 }
