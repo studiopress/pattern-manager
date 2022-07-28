840db0622a152263405ca8ed643345d7540182e2
Michael Day <michael.day@wpengine.com>
2022-07-27 22:55:47 -0500

    Add formatting options for file to be written.

--- wp-modules/theme-json-data-handlers/theme-json-data-handlers.php
+++ wp-modules/theme-json-data-handlers/theme-json-data-handlers.php
@@ -16,12 +16,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * Save theme style variation.
+ *
+ * @param  array  $style  The style variation to be saved.
+ * @return bool
+ */
 function update_theme_style( $style ) {
 
-	$style_default = array(
+	$style_default = [
 		'id'	=> '',
-		'body' 	=> [],
-	);
+		'title' => '',
+		'body'  => [],
+	];
 
 	$style = wp_parse_args( $style, $style_default );
 
@@ -30,21 +37,78 @@ function update_theme_style( $style ) {
 
 	$wp_theme_dir = get_template_directory();
 
-	if ( ! isset( $style['styles'] ) ) {
-		$styles_dir = $wp_theme_dir . '/styles/';
-		$style_body = json_encode( $style, JSON_PRETTY_PRINT );
-		$file_name  = sanitize_title( $style['id'] ) . '.php';
-	}
+	$styles_dir = $wp_theme_dir . '/styles/';
+	$file_body  = get_json_formatted_style( $style );
+	$file_name  = sanitize_title( $style['id'] ) . '.json'; // Use style id as filename.
 
+	// Add the styles dir if if does not exist.
 	if ( ! $wp_filesystem->exists( $styles_dir ) ) {
 		$wp_filesystem->mkdir( $styles_dir );
 	}
 
+	// Write the file to disk.
 	$style_file_created = $wp_filesystem->put_contents(
 		$styles_dir . $file_name,
-		$style_body,
+		$file_body,
 		FS_CHMOD_FILE
 	);
 
 	return $style_file_created;
+}
+
+/**
+ * Format the theme style variation for saving to [file].json.
+ *
+ * @param  array  $style  The style variation to be formatted.
+ * @return string
+ */
+function get_json_formatted_style( $style ) {
+	$style_json = [];
+
+	// Store the id and title to the new array.
+	$style_json['id'] 	 = $style['id'];
+	$style_json['title'] = $style['title'];
+
+	// Use array_merge so the body is saved on top-level.
+	$style_json = array_merge( $style_json, $style['body'] );
+
+	// Format style variation as site-editor prefers.
+	$style_json = unset_style_body_options( $style_json );
+
+	return json_encode( $style_json, JSON_PRETTY_PRINT );
+}
+
+/**
+ * Unset options that expose a gutenberg bug.
+ *
+ * Certain options being present in the json file cause selected style to not
+ * maintain selected border around style variation item in editor.
+ *
+ * @param  array  $style  The style variation eith options to be unset.
+ * @return array
+ */
+function unset_style_body_options( $style ) {
+	// Top level items.
+	unset( $style['customTemplates'] );
+	unset( $style['templateParts'] );
+
+	// Settings items.
+	unset( $style['settings']['color']['background'] );
+	unset( $style['settings']['color']['custom'] );
+	unset( $style['settings']['color']['customDuotone'] );
+	unset( $style['settings']['color']['customGradient'] );
+	unset( $style['settings']['color']['defaultGradients'] );
+	unset( $style['settings']['color']['defaultPalette'] );
+	unset( $style['settings']['color']['defaultDuotone'] );
+	unset( $style['settings']['color']['link'] );
+
+	unset( $style['settings']['typography']['customFontSize'] );
+	unset( $style['settings']['typography']['dropCap'] );
+	unset( $style['settings']['typography']['fontStyle'] );
+	unset( $style['settings']['typography']['fontWeight'] );
+	unset( $style['settings']['typography']['letterSpacing'] );
+	unset( $style['settings']['typography']['textDecoration'] );
+	unset( $style['settings']['typography']['textTransform'] );
+
+	return $style;
 }
