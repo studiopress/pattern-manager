04ecd7480c7b0bc86ff35d8a5b70f734fd81c67e
Michael Day <michael.day@wpengine.com>
2022-08-09 09:35:18 -0500

    Fix a typo

--- wp-modules/app/js/src/components/ThemeJsonEditor/index.js
+++ wp-modules/app/js/src/components/ThemeJsonEditor/index.js
@@ -161,7 +161,7 @@ export default function ThemeJsonEditor( { visible } ) {
 								<div className="flex flex-col gap-2">
 									<div>
 										<label
-											htmlFor="themes"
+											htmlFor="style-variations"
 											className="block text-sm font-medium text-gray-700 visuallyhidden"
 										>
 											{ __( 'Choose a style variation', 'fse-studio' ) }
