98f1ae352b46dda0f8d42cc3cce528dd2e27b278
Mike McAlister <m@mikemcalister.com>
2022-08-18 15:18:43 -0500

    Landing page boxes markup

--- wp-modules/app/js/src/components/ThemeSetup/index.js
+++ wp-modules/app/js/src/components/ThemeSetup/index.js
@@ -6,6 +6,13 @@ import { Icon, check } from '@wordpress/icons';
 import useStudioContext from '../../hooks/useStudioContext';
 import createNewTheme from '../../utils/createNewTheme';
 
+import imgThemeTemplate from '../../../../img/theme-templates.svg';
+import imgThemeTemplateParts from '../../../../img/theme-template-parts.svg';
+import imgThemePatterns from '../../../../img/theme-patterns.svg';
+import imgThemeJson from '../../../../img/theme-themejson.svg';
+import imgThemeStyleVariations from '../../../../img/theme-style-variations.svg';
+import imgThemeExport from '../../../../img/theme-export.svg';
+
 /** @param {{isVisible: boolean}} props */
 export default function ThemeSetup( { isVisible } ) {
 	const { currentTheme, themes, currentThemeId, currentView } =
@@ -75,6 +82,232 @@ export default function ThemeSetup( { isVisible } ) {
 			</div>
 
 			<div className="mx-auto p-8 lg:p-12">
+				<div className="mb-48 mt-12 max-w-7xl mx-auto">
+					<div className="grid grid-cols-1 gap-y-10 gap-x-10 sm:grid-cols-2 lg:grid-cols-3 lg:gap-12">
+						
+						<div className="group bg-fses-gray rounded-md flex justify-between flex-col">
+							<div className="w-full p-10 rounded-t-md overflow-hidden bg-wp-blue pb-0">
+								<img className="w-full" src={ imgThemeTemplate } />
+							</div>
+							<div className="flex justify-between p-10 text-center">
+								<div className='flex flex-col gap-5'>
+									<h3 className="text-2xl font-semibold">
+										{ __(
+											'Theme Templates',
+											'fse-studio'
+										) }
+									</h3>
+									<p className="text-base">
+										{ __(
+											'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+											'fse-studio'
+										) }
+									</p>
+									<button
+										type="button"
+										className="text-wp-blue hover:underline font-medium"
+										onClick={ () => {
+											currentView.set(
+												'theme_templates'
+											);
+										} }
+									>
+										{ __(
+											'Edit Theme Templates →',
+											'fse-studio'
+										) }
+									</button>
+								</div>
+							</div>
+							<div className="bg-[#E9E8E8] text-center p-4 text-gray-500 rounded-b-md">Your theme has no templates</div>
+						</div>
+
+						<div className="group bg-fses-gray rounded-md flex justify-between flex-col">
+							<div className="w-full p-10 rounded-t-md overflow-hidden bg-wp-blue pb-0">
+								<img className="w-full" src={ imgThemeTemplateParts } />
+							</div>
+							<div className="flex justify-between p-10 text-center">
+								<div className='flex flex-col gap-5'>
+									<h3 className="text-2xl font-semibold">
+										{ __(
+											'Template Parts',
+											'fse-studio'
+										) }
+									</h3>
+									<p className="text-base">
+										{ __(
+											'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+											'fse-studio'
+										) }
+									</p>
+									<button
+										type="button"
+										className="text-wp-blue hover:underline font-medium"
+										onClick={ () => {
+											currentView.set(
+												'template_parts'
+											);
+										} }
+									>
+										{ __(
+											'Edit Template Parts →',
+											'fse-studio'
+										) }
+									</button>
+								</div>
+							</div>
+							<div className="bg-[#E9E8E8] text-center p-4 text-gray-500 rounded-b-md">Your theme has no template parts</div>
+						</div>
+
+						<div className="group bg-fses-gray rounded-md flex justify-between flex-col">
+							<div className="w-full p-10 rounded-t-md overflow-hidden bg-wp-blue">
+								<img className="w-full" src={ imgThemePatterns } />
+							</div>
+							<div className="flex justify-between p-10 text-center">
+								<div className='flex flex-col gap-5'>
+									<h3 className="text-2xl font-semibold">
+										{ __(
+											'Theme Patterns',
+											'fse-studio'
+										) }
+									</h3>
+									<p className="text-base">
+										{ __(
+											'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+											'fse-studio'
+										) }
+									</p>
+									<button
+										type="button"
+										className="text-wp-blue hover:underline font-medium"
+										onClick={ () => {
+											currentView.set(
+												'theme_patterns'
+											);
+										} }
+									>
+										{ __(
+											'Edit Theme Patterns →',
+											'fse-studio'
+										) }
+									</button>
+								</div>
+							</div>
+							<div className="bg-[#E9E8E8] text-center p-4 text-gray-500 rounded-b-md">Your theme has no patterns</div>
+						</div>
+
+						<div className="group bg-fses-gray rounded-md flex justify-between flex-col">
+							<div className="w-full p-10 rounded-t-md overflow-hidden bg-wp-blue pb-0">
+								<img className="w-full" src={ imgThemeJson } />
+							</div>
+							<div className="flex justify-between p-10 text-center">
+								<div className='flex flex-col gap-5'>
+									<h3 className="text-2xl font-semibold">
+										{ __(
+											'Theme.json',
+											'fse-studio'
+										) }
+									</h3>
+									<p className="text-base">
+										{ __(
+											'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+											'fse-studio'
+										) }
+									</p>
+									<button
+										type="button"
+										className="text-wp-blue hover:underline font-medium"
+										onClick={ () => {
+											currentView.set(
+												'themejson_editor'
+											);
+										} }
+									>
+										{ __(
+											'Edit Theme.json →',
+											'fse-studio'
+										) }
+									</button>
+								</div>
+							</div>
+							<div className="bg-[#E9E8E8] text-center p-4 text-gray-500 rounded-b-md">Your theme.json file is unedited</div>
+						</div>
+
+						<div className="group bg-fses-gray rounded-md flex justify-between flex-col">
+							<div className="w-full p-10 rounded-t-md overflow-hidden bg-wp-blue">
+								<img className="w-full max-w-[260px] mx-auto" src={ imgThemeStyleVariations } />
+							</div>
+							<div className="flex justify-between p-10 text-center">
+								<div className='flex flex-col gap-5'>
+									<h3 className="text-2xl font-semibold">
+										{ __(
+											'Style Variations',
+											'fse-studio'
+										) }
+									</h3>
+									<p className="text-base">
+										{ __(
+											'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+											'fse-studio'
+										) }
+									</p>
+									<button
+										type="button"
+										className="text-wp-blue hover:underline font-medium"
+										onClick={ () => {
+											currentView.set(
+												'themejson_editor'
+											);
+										} }
+									>
+										{ __(
+											'Edit Style Variations →',
+											'fse-studio'
+										) }
+									</button>
+								</div>
+							</div>
+							<div className="bg-[#E9E8E8] text-center p-4 text-gray-500 rounded-b-md">Your theme has no style variations</div>
+						</div>
+
+						<div className="group bg-fses-gray rounded-md flex justify-between flex-col">
+							<div className="w-full p-10 rounded-t-md overflow-hidden bg-wp-blue">
+								<img className="w-full max-w-[260px] mx-auto" src={ imgThemeExport } />
+							</div>
+							<div className="flex justify-between p-10 text-center">
+								<div className='flex flex-col gap-5'>
+									<h3 className="text-2xl font-semibold">
+										{ __(
+											'Download Theme Zip',
+											'fse-studio'
+										) }
+									</h3>
+									<p className="text-base">
+										{ __(
+											'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+											'fse-studio'
+										) }
+									</p>
+									<button
+										type="button"
+										className="text-wp-blue hover:underline font-medium"
+										onClick={ () => {
+											currentTheme.export();
+										} }
+									>
+										{ __(
+											'Download Theme →',
+											'fse-studio'
+										) }
+									</button>
+								</div>
+							</div>
+							<div className="bg-[#E9E8E8] text-center p-4 text-gray-500 rounded-b-md">You can also view your theme files</div>
+						</div>
+
+					</div>
+				</div>
+
 				<form className="max-w-7xl mx-auto flex flex-wrap justify-between gap-10 lg:gap-20">
 					<div className="flex-initial w-full md:w-2/3">
 						{ currentTheme.existsOnDisk ? (
