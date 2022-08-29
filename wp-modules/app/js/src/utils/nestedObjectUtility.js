7443cf9bf10beb8e1054053fc304a9a5ce876a37
Michael Day <michael.day@wpengine.com>
2022-08-29 17:16:52 -0500

    Add documentation, make util funcs more verbose.

--- wp-modules/app/js/src/utils/nestedObjectUtility.js
+++ wp-modules/app/js/src/utils/nestedObjectUtility.js
@@ -9,23 +9,23 @@
  * @return {*} The nested value.
  */
 export function getNestedValue( object, keys ) {
+	// Iterate over `keys`, passing the `currentKey` as the `accumulator` index on each iteration.
 	return keys.reduce(
-		( acc, key ) => ( acc ? acc[ key ] : undefined ),
-		object
+		( accumulator, currentKey ) =>
+			accumulator ? accumulator[ currentKey ] : undefined,
+		object // Initial value for the accumulator.
 	);
 }
 
 /**
  * Update a deeply nested object value.
  *
- * The initial function accepts a value before recursively cloning the
- * object upon secondary arguments being passed immediately after.
- *
- * For example, the partial application could be called like this:
+ * The initial function accepts a value before recursively cloning the object upon secondary args
+ * being passed immediately after. For example, the partial application could be called like this:
  *  - setNestedObject( value, defaultValue )( object, keys )
  *
- * @param {*} value
- * @param {*} defaultValue
+ * @param {*} value        A `value` of null means the child element should be deleted.
+ * @param {*} defaultValue Equality of `defaultValue` and `value` also specifies deletion.
  * @return {Function} recursiveUpdate()
  */
 export function setNestedObject( value, defaultValue ) {
@@ -36,34 +36,43 @@ export function setNestedObject( value, defaultValue ) {
 	 * @param {Array}  keys
 	 * @return {Object} The new, updated object.
 	 */
-	return function recursiveUpdate(
-		object,
-		[ firstKey, ...theRestOfTheKeys ] = []
-	) {
+	return function recursiveUpdate( object, keys = [] ) {
+		// Destructure the keys.
+		const [ currentKey, ...theRestOfTheKeys ] = keys;
+
+		// Create a new object to avoid mutating the original.
+		// Check if the current level is an array or object, then spread accordingly.
 		const newObject = Array.isArray( object )
 			? [ ...object ]
 			: { ...object };
 
 		if (
-			// This means we are at the end of the keys.
+			// `! theRestOfTheKeys.length` means we are at the end of the keys.
+			// If `value` is null or equal to `defaultValue`, we need to delete the element.
 			! theRestOfTheKeys.length &&
 			( null === value ||
 				( defaultValue !== null && defaultValue === value ) )
 		) {
-			// Delete the element.
-			delete newObject[ firstKey ];
+			delete newObject[ currentKey ];
 		} else {
-			// Recurse if there are more keys, otherwise update the element.
-			newObject[ firstKey ] = theRestOfTheKeys.length
-				? recursiveUpdate( object[ firstKey ], theRestOfTheKeys )
+			// If there are keys left in `theRestOfTheKeys`, clone the next level of the object.
+			// If there are no keys left, we've reached the target level!
+			// Store the result of either condition on a new `currentKey` index.
+			newObject[ currentKey ] = theRestOfTheKeys.length
+				? recursiveUpdate( object[ currentKey ], theRestOfTheKeys )
 				: value;
 		}
 
-		// Filter out deleted elements from parent arrays.
-		return Array.isArray( newObject ) &&
+		if (
+			// Filter out empty elements from parent arrays of deleted children.
+			// If the `value` is null or equal to `defaultValue`, we need to clean up the parent.
+			Array.isArray( newObject ) &&
 			( null === value ||
 				( defaultValue !== null && defaultValue === value ) )
-			? newObject.filter( Boolean )
-			: newObject;
+		) {
+			newObject.filter( Boolean );
+		}
+
+		return newObject;
 	};
 }
