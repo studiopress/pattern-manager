9c9f612ec6eb010d8d704cbe8c9f60592946ab5d
Ryan Kienstra <kienstraryan@gmail.com>
2022-08-31 16:44:36 -0500

    Add a description for the notice context

--- wp-modules/app/js/src/hooks/useNoticeContext.js
+++ wp-modules/app/js/src/hooks/useNoticeContext.js
@@ -3,7 +3,7 @@
 import { useContext } from '@wordpress/element';
 import NoticeContext from '../contexts/FseStudioNoticeContext';
 
-/** @return {ReturnType<import('./useNotice')>} */
+/** @return {ReturnType<import('./useNotice')>} The notice context. */
 export default function useNoticeContext() {
 	const context = useContext( NoticeContext );
 	if ( ! context ) {
