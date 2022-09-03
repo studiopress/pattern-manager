9c9c2eb2ce2bb2cd52b090b8135396c9f4ebca2c
Ryan Kienstra <kienstraryan@gmail.com>
2022-09-03 14:55:49 -0500

    Ignore a false positive linting error in 2 files
    According to the Mozilla documentation,
    path[fill] is valid:
    https://developer.mozilla.org/en-US/docs/Web/SVG/Element/path

--- wp-modules/app/js/src/components/ThemeSetup/ThemeCreatedNotice.js
+++ wp-modules/app/js/src/components/ThemeSetup/ThemeCreatedNotice.js
@@ -1,3 +1,5 @@
+/* eslint-ignore react/no-unknown-property */
+
 import { useEffect, useRef } from '@wordpress/element';
 import { Icon, close } from '@wordpress/icons';
 import { __ } from '@wordpress/i18n';
@@ -25,13 +27,11 @@ export default function ThemeCreatedNotice( { onDismiss } ) {
 						fillRule="evenodd"
 						clipRule="evenodd"
 						d="M15.2071 8.29289C15.5976 8.68342 15.5976 9.31658 15.2071 9.70711L10.7071 14.2071C10.3166 14.5976 9.68342 14.5976 9.29289 14.2071L6.79289 11.7071C6.40237 11.3166 6.40237 10.6834 6.79289 10.2929C7.18342 9.90237 7.81658 9.90237 8.20711 10.2929L10 12.0858L13.7929 8.29289C14.1834 7.90237 14.8166 7.90237 15.2071 8.29289Z"
-						fill="#35872F"
 					/>
 					<path
 						fillRule="evenodd"
 						clipRule="evenodd"
 						d="M11 2C6.02944 2 2 6.02944 2 11C2 15.9706 6.02944 20 11 20C15.9706 20 20 15.9706 20 11C20 6.02944 15.9706 2 11 2ZM0 11C0 4.92487 4.92487 0 11 0C17.0751 0 22 4.92487 22 11C22 17.0751 17.0751 22 11 22C4.92487 22 0 17.0751 0 11Z"
-						fill="#35872F"
 					/>
 				</svg>
 			</span>
