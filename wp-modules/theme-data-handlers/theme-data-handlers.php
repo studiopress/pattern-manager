2d483172a959d0d4cb41a41ceeea77ecdc5e2faf
Ryan Kienstra <kienstraryan@gmail.com>
2022-09-19 22:38:31 -0500

    Remove the true recursive argument
    If $wp_filesystem->move() worked,
    it should have moved all of the files.
    There shouldn't be a need to recursively
    rm the previous theme dir.

--- wp-modules/theme-data-handlers/theme-data-handlers.php
+++ wp-modules/theme-data-handlers/theme-data-handlers.php
@@ -177,7 +177,7 @@ function update_theme( $theme ) {
 			$wp_filesystem->move( $previous_theme_file, str_replace( $previous_theme_dir, $new_theme_dir, $previous_theme_file ) );
 		}
 
-		$wp_filesystem->rmdir( $previous_theme_dir, true );
+		$wp_filesystem->rmdir( $previous_theme_dir );
 	}
 
 	// Create the new theme directory, if it does not already exist.
