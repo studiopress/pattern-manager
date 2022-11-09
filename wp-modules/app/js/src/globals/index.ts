67313c4c9c6029f52bc978e260ec4be5332b7839
Michael Day <michael.day@wpengine.com>
2022-11-09 15:35:04 -0600

    Convert globals file to TS with InitialFseStudio

---
+++ wp-modules/app/js/src/globals/index.ts
@@ -0,0 +1,27 @@
+import { Pattern, Theme } from '../types';
+
+type InitialFseStudio = {
+	apiNonce: string,
+	apiEndpoints: {
+		getAppState: string,
+		getPatternEndpoint: string,
+		getThemeEndpoint: string,
+		getThemeJsonFileEndpoint: string,
+		savePatternEndpoint: string,
+		saveThemeEndpoint: string,
+		switchThemeEndpoint: string,
+		saveThemeJsonFileEndpoint: string,
+		exportThemeEndpoint: string,
+	},
+	blockEditorSettings: Partial<{
+		'__unstableResolvedAssets': { styles: string },
+		styles: Record<string, unknown>[]
+	}>,
+	initialTheme: string,
+	patterns: { [ key: string ]: Pattern },
+	siteUrl: string,
+	adminUrl: string,
+	themes: { [ key: string ]: Theme }
+}
+
+export const fsestudio: InitialFseStudio = window.fsestudio;
