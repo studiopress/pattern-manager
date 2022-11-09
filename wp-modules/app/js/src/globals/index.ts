bac58dcb19fb388e33f66ce45f85403a02c7367f
Michael Day <michael.day@wpengine.com>
2022-11-09 16:19:45 -0600

    Linting fixes

--- wp-modules/app/js/src/globals/index.ts
+++ wp-modules/app/js/src/globals/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef, camelcase */
+
 import { Pattern, Theme } from '../types';
 
 declare global {
@@ -7,27 +9,27 @@ declare global {
 }
 
 type InitialFseStudio = {
-	apiNonce: string,
+	apiNonce: string;
 	apiEndpoints: {
-		getAppState: string,
-		getPatternEndpoint: string,
-		getThemeEndpoint: string,
-		getThemeJsonFileEndpoint: string,
-		savePatternEndpoint: string,
-		saveThemeEndpoint: string,
-		switchThemeEndpoint: string,
-		saveThemeJsonFileEndpoint: string,
-		exportThemeEndpoint: string,
-	},
-	blockEditorSettings: Partial<{
-		'__unstableResolvedAssets': { styles: string },
-		styles: Record<string, unknown>[]
-	}>,
-	initialTheme: string,
-	patterns: { [ key: string ]: Pattern },
-	siteUrl: string,
-	adminUrl: string,
-	themes: { [ key: string ]: Theme }
-}
+		getAppState: string;
+		getPatternEndpoint: string;
+		getThemeEndpoint: string;
+		getThemeJsonFileEndpoint: string;
+		savePatternEndpoint: string;
+		saveThemeEndpoint: string;
+		switchThemeEndpoint: string;
+		saveThemeJsonFileEndpoint: string;
+		exportThemeEndpoint: string;
+	};
+	blockEditorSettings: Partial< {
+		__unstableResolvedAssets: { styles: string };
+		styles: Record< string, unknown >[];
+	} >;
+	initialTheme: string;
+	patterns: { [ key: string ]: Pattern };
+	siteUrl: string;
+	adminUrl: string;
+	themes: { [ key: string ]: Theme };
+};
 
 export const fsestudio = window.fsestudio;
--- wp-modules/app/js/src/types.ts
+++ wp-modules/app/js/src/types.ts
@@ -1,4 +1,4 @@
-/* eslint-disable no-undef */
+/* eslint-disable no-undef, camelcase */
 
 export type Pattern = {
 	type: string;
@@ -10,30 +10,30 @@ export type Pattern = {
 };
 
 export type Theme = {
-	id: string,
-	name: string,
-	namespace: string,
-	'index.html'?: string,
-	'404.html'?: string,
-	'archive.html'?: string,
-	'single.html'?: string,
-	'page.html'?: string,
-	'search.html'?: string,
-	author: string,
-	author_uri: string,
-	description: string,
-	dirname: string,
-	included_patterns?: { [ key: string ]: Pattern },
-	requires_php: string,
-	requires_wp: string,
-	rest_route?: string,
-	styles: Object,
-	tags: string,
-	template_files: string[],
-	template_parts: string[],
-	tested_up_to: string,
-	text_domain: string,
-	theme_json_file: string[],
-	uri: string,
-	version: string
-}
+	id: string;
+	name: string;
+	namespace: string;
+	'index.html'?: string;
+	'404.html'?: string;
+	'archive.html'?: string;
+	'single.html'?: string;
+	'page.html'?: string;
+	'search.html'?: string;
+	author: string;
+	author_uri: string;
+	description: string;
+	dirname: string;
+	included_patterns?: { [ key: string ]: Pattern };
+	requires_php: string;
+	requires_wp: string;
+	rest_route?: string;
+	styles: Object;
+	tags: string;
+	template_files: string[];
+	template_parts: string[];
+	tested_up_to: string;
+	text_domain: string;
+	theme_json_file: string[];
+	uri: string;
+	version: string;
+};
