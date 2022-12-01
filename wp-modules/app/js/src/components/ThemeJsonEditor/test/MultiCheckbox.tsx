15d39bfc330af567ec8a59c9d9db4eadb64c6f9a
Michael Day <michael.day@wpengine.com>
2022-12-01 15:08:26 -0600

    Convert MultiCheckbox test to TSX

--- wp-modules/app/js/src/components/ThemeJsonEditor/test/MultiCheckbox.tsx
+++ wp-modules/app/js/src/components/ThemeJsonEditor/test/MultiCheckbox.tsx
@@ -1,8 +1,9 @@
-// @ts-check
+/* eslint-disable no-unused-vars, no-undef */
 
 import { useState } from '@wordpress/element';
 import { fireEvent, render, screen } from '@testing-library/react';
 import MultiCheckbox from '../MultiCheckbox';
+import { ButtonHTMLAttributes } from 'react';
 
 function Container( { initialValue } ) {
 	const [ checkboxValue, setCheckboxValue ] = useState( initialValue );
@@ -18,13 +19,15 @@ describe( 'MultiCheckbox', () => {
 	it.each( units )( 'should have checked checkboxes', ( unit ) => {
 		render( <Container initialValue={ units } /> );
 
-		expect( screen.getByLabelText( unit ).checked ).toEqual( true );
+		expect(
+			screen.getByLabelText< HTMLInputElement >( unit ).checked
+		).toEqual( true );
 	} );
 
 	it( 'should handle unchecking', () => {
 		render( <Container initialValue={ units } /> );
 
-		const checkbox = screen.getByLabelText( 'px' );
+		const checkbox: HTMLInputElement = screen.getByLabelText( 'px' );
 		expect( checkbox.checked ).toEqual( true );
 		fireEvent.click( checkbox );
 		expect( checkbox.checked ).toEqual( false );
