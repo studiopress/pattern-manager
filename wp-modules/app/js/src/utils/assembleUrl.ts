fb89aaab716e30078888f3cac876153fe73d1911
Michael Day <michael.day@wpengine.com>
2022-12-02 14:44:22 -0600

    Add some docs again for assembleUrl, classNames

--- wp-modules/app/js/src/utils/assembleUrl.ts
+++ wp-modules/app/js/src/utils/assembleUrl.ts
@@ -1,3 +1,12 @@
+/* eslint-disable jsdoc/require-param-type, jsdoc/require-returns-type */
+
+/**
+ * Assemble a URL given the base/relative URL and search parameters.
+ *
+ * @param  theUrl
+ * @param  params
+ * @return The full URL.
+ */
 export default function assembleUrl(
 	theUrl: string,
 	params: Record< string, string >
