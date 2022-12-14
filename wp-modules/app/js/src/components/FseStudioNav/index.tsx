653ece6c6861609dad6b09beb079d158abb02af7
Ryan Kienstra <kienstraryan@gmail.com>
2022-12-14 13:47:12 -0600

    Remove a param tag that adds nothing

--- wp-modules/app/js/src/components/FseStudioNav/index.tsx
+++ wp-modules/app/js/src/components/FseStudioNav/index.tsx
@@ -35,8 +35,6 @@ export default function FseStudioNav() {
 	 *
 	 * Main purpose here is to throw notice for user to refresh editor.
 	 * Currently targets pattern and template editors.
-	 *
-	 * @param {string} message
 	 */
 	function maybePostWindowMessage( message: string ) {
 		if ( patternEditorIframe?.current ) {
