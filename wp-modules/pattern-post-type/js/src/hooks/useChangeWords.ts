22873ce8140a1a4aa1b94f43399256ca56e5a25a
Michael Day <michael.day@wpengine.com>
2022-12-20 12:37:49 -0600

    Move changeWords to hook

---
+++ wp-modules/pattern-post-type/js/src/hooks/useChangeWords.ts
@@ -0,0 +1,49 @@
+export default function useChangeWords( { postMeta } ) {
+	// Change the word "Publish" to "Save Pattern"
+	function changeWords( translation, text ) {
+		if ( postMeta?.type === 'pattern' ) {
+			if ( text === 'Publish' ) {
+				return 'Save pattern to theme';
+			}
+			if ( text === 'Post published.' || text === 'Post updated.' ) {
+				return 'Pattern saved to theme';
+			}
+			if ( text === 'Update' || text === 'Post updated.' ) {
+				return 'Update Pattern';
+			}
+			if ( text === 'Add New Tag' ) {
+				return 'Pattern Categories';
+			}
+			if ( text === 'Saved' ) {
+				return 'Saved to your theme directory';
+			}
+		}
+
+		if ( postMeta?.type === 'template' ) {
+			if ( text === 'Pattern' ) {
+				return 'Template';
+			}
+			if ( text === 'Publish' ) {
+				return 'Save template to theme';
+			}
+			if ( text === 'Post published.' || text === 'Post updated.' ) {
+				return 'Template saved to theme';
+			}
+			if ( text === 'Update' || text === 'Post updated.' ) {
+				return 'Update Template';
+			}
+			if ( text === 'Add New Tag' ) {
+				return 'Pattern Categories';
+			}
+			if ( text === 'Saved' ) {
+				return 'Saved to your theme directory';
+			}
+		}
+
+		return translation;
+	}
+
+	return {
+		changeWords,
+	};
+}
