cabb7dff3b2a66b56558ea3ba827eca557ba6ce9
Michael Day <michael.day@wpengine.com>
2022-12-22 17:10:14 -0600

    Cleanup utils

--- wp-modules/pattern-post-type/js/src/utils/sortAlphabetically.ts
+++ wp-modules/pattern-post-type/js/src/utils/sortAlphabetically.ts
@@ -1,30 +1,12 @@
-/**
- * Sort an array of objects alphabetically by key.
- * Optionally, include a key and string to place items on top of the sorted array.
- *
- * @param {Array}  arr       The array for sorting.
- * @param {string} key       The key to use for sorting.
- * @param {string} topKey    The extra key to check for pushing items to the top.
- * @param {string} topString The extra string to match for pushing items to the top.
- * @return {Array}           The sorted array.
- */
-export default function sortAlphabetically(
-	arr: any[],
-	key: string,
-	topKey = '',
-	topString = ''
+/** Sorts an array of objects alphabetically by key. */
+export default function sortAlphabetically< T extends unknown[] >(
+	arr: T,
+	key: string | number
 ) {
 	// Sort the objects alphabetically by given key.
 	arr.sort( ( a, b ) => {
 		return a[ key ] > b[ key ] ? 1 : -1;
 	} );
-
-	// Check the extra key and string for pushing items to top.
-	if ( topKey && topString ) {
-		arr.sort( ( a ) => {
-			return a[ topKey ] === topString ? -1 : 0;
-		} );
-	}
 
 	return arr;
 }
