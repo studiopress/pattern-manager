8a2850ab61350507a529091a5b86d9ee67af2a04
Michael Day <michael.day@wpengine.com>
2023-01-03 10:40:10 -0600

    Re-add flatUnorderedEquals utility

---
+++ wp-modules/pattern-post-type/js/src/utils/flatUnorderedEquals.ts
@@ -0,0 +1,14 @@
+/** Check if two flat arrays are loosely equal. */
+export default function flatUnorderedEquals<
+	T extends string | number | boolean
+>( arrayA: T[], arrayB: T[] ) {
+	arrayA.sort();
+	arrayB.sort();
+
+	return (
+		arrayA.length === arrayB.length &&
+		arrayA.every( ( value, index ) => {
+			return value === arrayB[ index ];
+		} )
+	);
+}
