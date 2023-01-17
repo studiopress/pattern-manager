13c190bd3726705d3040c4a14171159b796fa257
Ryan Kienstra <kienstraryan@gmail.com>
2023-01-17 12:13:37 -0600

    Remove types that aren't needed

--- wp-modules/app/js/src/hooks/useCurrentView.ts
+++ wp-modules/app/js/src/hooks/useCurrentView.ts
@@ -1,9 +1,6 @@
 import { useState } from '@wordpress/element';
 import { PatternManagerViews } from '../types';
 
-type PatternsView = 'theme_patterns' | 'pattern_editor';
-type TemplatesView = 'theme_templates' | 'pattern_editor';
-
 export default function useCurrentView( initialView: PatternManagerViews ) {
 	const [ currentView, setCurrentView ] = useState( initialView );
 
