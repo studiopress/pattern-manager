90c9244a6f281355c8e6aebc57b6affc78e32c97
Michael Day <michael.day@wpengine.com>
2023-01-17 20:47:39 -0600

    Package.json, barrel exports for app utils

---
+++ wp-modules/app/js/src/utils/index.ts
@@ -0,0 +1,6 @@
+export { default as convertToPascalCase } from './convertToPascalCase';
+export { default as convertToSlug } from './convertToSlug';
+export { default as convertToUpperCase } from './convertToUpperCase';
+export { default as flatUnorderedEquals } from './flatUnorderedEquals';
+export { default as searchItems } from './searchItems';
+export { default as sortAlphabetically } from './sortAlphabetically';
