1dd171a707ef20c9faecb38157d5f4bea6c80439
Ryan Kienstra <kienstraryan@gmail.com>
2023-01-23 14:58:17 -0600

    Only show top bar on the Patterns view

--- wp-modules/app/js/src/components/App/index.tsx
+++ wp-modules/app/js/src/components/App/index.tsx
@@ -68,68 +68,70 @@ function PatternManager() {
 					{ notice.snackBarValue }
 				</Snackbar>
 			) : null }
-			<div className="patternmanager-nav-container">
-				<div className="nav-container-logo">
-					<img
-						className="logo-svg"
-						aria-hidden="true"
-						alt=""
-						src={ wpeLogoDefaultCropped }
-					/>
-					<span className="logo-title">
-						{ __( 'Pattern Manager', 'pattern-manager' ) }
-					</span>
-				</div>
-
-				<div className="nav-container-inner">
-					<button
-						type="button"
-						disabled={ patterns.fetchInProgress }
-						className="nav-button"
-						onClick={ () => {
-							patterns.save();
-						} }
-					>
-						{ patterns.isSaving ? (
-							<>
-								<Spinner />
-								{ __( 'Saving', 'pattern-manager' ) }
-							</>
-						) : (
-							__( 'Save', 'pattern-manager' )
-						) }
-					</button>
-
-					<button
-						className="nav-button"
-						onClick={ () => {
-							// Get the new pattern title and slug.
-							const { patternTitle, patternSlug } =
-								getNextPatternIds( patterns.data );
-
-							patterns.createPattern( {
-								title: patternTitle,
-								name: patternSlug,
-								slug: patternSlug,
-								categories: [],
-								keywords: [],
-								blockTypes: [],
-								postTypes: [],
-								inserter: true,
-								description: '',
-								viewportWidth: '',
-								content: '',
-							} );
-							currentPatternId.set( patternSlug );
-							currentView.set( 'pattern_editor' );
-						} }
-					>
-						{ __( 'Create New Pattern', 'pattern-manager' ) }
-					</button>
-				</div>
-			</div>
 			{ 'theme_patterns' === currentView.currentView ? (
-				<Patterns />
+				<>
+					<div className="patternmanager-nav-container">
+						<div className="nav-container-logo">
+							<img
+								className="logo-svg"
+								aria-hidden="true"
+								alt=""
+								src={ wpeLogoDefaultCropped }
+							/>
+							<span className="logo-title">
+								{ __( 'Pattern Manager', 'pattern-manager' ) }
+							</span>
+						</div>
+
+						<div className="nav-container-inner">
+							<button
+								type="button"
+								disabled={ patterns.fetchInProgress }
+								className="nav-button"
+								onClick={ () => {
+									patterns.save();
+								} }
+							>
+								{ patterns.isSaving ? (
+									<>
+										<Spinner />
+										{ __( 'Saving', 'pattern-manager' ) }
+									</>
+								) : (
+									__( 'Save', 'pattern-manager' )
+								) }
+							</button>
+
+							<button
+								className="nav-button"
+								onClick={ () => {
+									// Get the new pattern title and slug.
+									const { patternTitle, patternSlug } =
+										getNextPatternIds( patterns.data );
+
+									patterns.createPattern( {
+										title: patternTitle,
+										name: patternSlug,
+										slug: patternSlug,
+										categories: [],
+										keywords: [],
+										blockTypes: [],
+										postTypes: [],
+										inserter: true,
+										description: '',
+										viewportWidth: '',
+										content: '',
+									} );
+									currentPatternId.set( patternSlug );
+									currentView.set( 'pattern_editor' );
+								} }
+							>
+								{ __( 'Create New Pattern', 'pattern-manager' ) }
+							</button>
+						</div>
+					</div>
+					<Patterns />
+				</>
 			) : null }
 			{ 'pattern_editor' === currentView.currentView ? (
 				<PatternEditor />
