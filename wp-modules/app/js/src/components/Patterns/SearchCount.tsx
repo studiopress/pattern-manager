1500575f3ef0c60f3cc54ac8c39984cdd5dbf258
Michael Day <michael.day@wpengine.com>
2023-01-24 14:45:34 -0600

    Add the missing quotation marks

--- wp-modules/app/js/src/components/Patterns/SearchCount.tsx
+++ wp-modules/app/js/src/components/Patterns/SearchCount.tsx
@@ -15,8 +15,8 @@ export default function SearchCount( { resultsLength, searchTerm }: Props ) {
 				{ sprintf(
 					/* translators: %1$d: the number of patterns found, %2$s: the search term for patterns */
 					_n(
-						'%1$d pattern found for %2$s',
-						'%1$d patterns found for %2$s',
+						'%1$d pattern found for "%2$s"',
+						'%1$d patterns found for "%2$s"',
 						resultsLength,
 						'pattern-manager'
 					),
