4b8cfbf02d3e5364306d251791bc0fe28030d7a0
Ryan Kienstra <kienstraryan@gmail.com>
2023-01-23 20:43:52 -0600

    Move header into its own file

--- wp-modules/app/js/src/components/App/index.tsx
+++ wp-modules/app/js/src/components/App/index.tsx
@@ -1,6 +1,5 @@
 //  Assets
 import '../../../../css/src/index.scss';
-import wpeLogoDefaultCropped from '../../../../img/WPE-LOGO-S-Default-Cropped.svg';
 
 // WP dependencies
 import { useRef } from '@wordpress/element';
@@ -21,11 +20,9 @@ import usePmContext from '../../hooks/usePmContext';
 import useNotice from '../../hooks/useNotice';
 
 // Components
+import Header from '../Header';
 import Patterns from '../Patterns';
 import PatternEditor from '../PatternEditor';
-
-// Utils
-import getNextPatternIds from '../../utils/getNextPatternIds';
 
 // Types
 import type { InitialContext } from '../../types';
@@ -55,7 +52,7 @@ export default function App() {
 }
 
 function PatternManager() {
-	const { currentPatternId, currentView, notice, patterns } = usePmContext();
+	const { currentView, notice } = usePmContext();
 
 	return (
 		<>
@@ -70,69 +67,7 @@ function PatternManager() {
 			) : null }
 			{ 'theme_patterns' === currentView.currentView ? (
 				<>
-					<div className="patternmanager-nav-container">
-						<div className="nav-container-logo">
-							<img
-								className="logo-svg"
-								aria-hidden="true"
-								alt=""
-								src={ wpeLogoDefaultCropped }
-							/>
-							<span className="logo-title">
-								{ __( 'Pattern Manager', 'pattern-manager' ) }
-							</span>
-						</div>
-
-						<div className="nav-container-inner">
-							<button
-								type="button"
-								disabled={ patterns.fetchInProgress }
-								className="nav-button"
-								onClick={ () => {
-									patterns.save();
-								} }
-							>
-								{ patterns.isSaving ? (
-									<>
-										<Spinner />
-										{ __( 'Saving', 'pattern-manager' ) }
-									</>
-								) : (
-									__( 'Save', 'pattern-manager' )
-								) }
-							</button>
-
-							<button
-								className="nav-button"
-								onClick={ () => {
-									// Get the new pattern title and slug.
-									const { patternTitle, patternSlug } =
-										getNextPatternIds( patterns.data );
-
-									patterns.createPattern( {
-										title: patternTitle,
-										name: patternSlug,
-										slug: patternSlug,
-										categories: [],
-										keywords: [],
-										blockTypes: [],
-										postTypes: [],
-										inserter: true,
-										description: '',
-										viewportWidth: '',
-										content: '',
-									} );
-									currentPatternId.set( patternSlug );
-									currentView.set( 'pattern_editor' );
-								} }
-							>
-								{ __(
-									'Create New Pattern',
-									'pattern-manager'
-								) }
-							</button>
-						</div>
-					</div>
+					<Header />
 					<Patterns />
 				</>
 			) : null }
