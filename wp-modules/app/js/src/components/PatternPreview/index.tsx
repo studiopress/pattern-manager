bcef04f3aa0ce508baa0fd8aa60000be5825f24e
Michael Day <michael.day@wpengine.com>
2023-01-26 11:52:54 -0600

    Use the hook in PatternPreview

--- wp-modules/app/js/src/components/PatternPreview/index.tsx
+++ wp-modules/app/js/src/components/PatternPreview/index.tsx
@@ -4,6 +4,7 @@ import { __ } from '@wordpress/i18n';
 
 // Hooks
 import usePmContext from '../../hooks/usePmContext';
+import useLazyRender from '../../hooks/useLazyRender';
 
 type Props = {
 	url: string;
@@ -11,6 +12,27 @@ type Props = {
 };
 
 export default function PatternPreview( { url, scale }: Props ) {
+	const { lazyContainerRef, lazyHasIntersected } =
+		useLazyRender< HTMLDivElement >( {
+			threshold: [ 0.3, 0.6, 1.0 ],
+		} );
+
+	return (
+		<div
+			ref={ lazyContainerRef }
+			className="pattern-preview-iframe-outer"
+			style={ {
+				pointerEvents: 'none',
+			} }
+		>
+			{ lazyHasIntersected ? (
+				<PreviewIframe url={ url } scale={ scale } />
+			) : null }
+		</div>
+	);
+}
+
+function PreviewIframe( { url, scale }: Props ) {
 	const { patterns } = usePmContext();
 	const [ iframeRef, setIframeRef ] = useState<
 		HTMLIFrameElement | undefined
@@ -19,29 +41,22 @@ export default function PatternPreview( { url, scale }: Props ) {
 	const scaleMultiplier = 10 / ( scale * 10 );
 
 	return (
-		<div
-			className="pattern-preview-iframe-outer"
+		<iframe
+			src={ url }
+			title={ __( 'Pattern Preview', 'pattern-manager' ) }
+			role={ 'img' }
+			ref={ setIframeRef }
 			style={ {
+				position: 'absolute',
+				top: '0',
+				left: '0',
+				width: `${ 100 * scaleMultiplier }%`,
+				height: `${ 100 * scaleMultiplier }%`,
+				display: 'block',
+				transform: 'scale(' + scale + ')',
+				transformOrigin: 'top left',
 				pointerEvents: 'none',
 			} }
-		>
-			<iframe
-				src={ url }
-				title={ __( 'Pattern Preview', 'pattern-manager' ) }
-				role={ 'img' }
-				ref={ setIframeRef }
-				style={ {
-					position: 'absolute',
-					top: '0',
-					left: '0',
-					width: `${ 100 * scaleMultiplier }%`,
-					height: `${ 100 * scaleMultiplier }%`,
-					display: 'block',
-					transform: 'scale(' + scale + ')',
-					transformOrigin: 'top left',
-					pointerEvents: 'none',
-				} }
-			/>
-		</div>
+		/>
 	);
 }
