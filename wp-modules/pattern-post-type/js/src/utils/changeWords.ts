90d9119d2ba0b550ae328f128f48a7441e60bb9b
Ryan Kienstra <kienstraryan@gmail.com>
2023-01-30 22:29:21 -0600

    Internationalize the changeWords return values

--- wp-modules/pattern-post-type/js/src/utils/changeWords.ts
+++ wp-modules/pattern-post-type/js/src/utils/changeWords.ts
@@ -1,18 +1,20 @@
+import { __ } from '@wordpress/i18n';
+
 export default function changeWords( translation: string, text: string ) {
 	if ( text === 'Publish' ) {
-		return 'Save pattern to theme';
+		return __( 'Save pattern to theme', 'pattern-manager' );
 	}
 	if ( text === 'Post published.' || text === 'Post updated.' ) {
-		return 'Pattern saved to theme';
+		return __( 'Pattern saved to theme', 'pattern-manager' );
 	}
 	if ( text === 'Update' || text === 'Post updated.' ) {
-		return 'Update Pattern';
+		return __( 'Update Pattern', 'pattern-manager' );
 	}
 	if ( text === 'Add New Tag' ) {
-		return 'Pattern Categories';
+		return __( 'Pattern Categories', 'pattern-manager' );
 	}
 	if ( text === 'Saved' ) {
-		return 'Saved to your theme directory';
+		return __( 'Saved to your theme directory', 'pattern-manager' );
 	}
 
 	return translation;
