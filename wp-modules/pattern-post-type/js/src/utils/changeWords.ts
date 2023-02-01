959cfd23a724da8d5959d63d0be7da68553d0f21
Ryan Kienstra <kienstraryan@gmail.com>
2023-01-31 22:11:17 -0600

    Remove the textdomain arugment

--- wp-modules/pattern-post-type/js/src/utils/changeWords.ts
+++ wp-modules/pattern-post-type/js/src/utils/changeWords.ts
@@ -1,19 +1,21 @@
 import { __ } from '@wordpress/i18n';
 
 export default function changeWords( translation: string, text: string ) {
-	if ( text === __( 'Publish', 'pattern-manager' ) ) {
+	// Doesn't have a textdomain argument so that it will use the Core textdomain.
+	// This filter should get receive translations from Core, not this plugin.
+	if ( text === __( 'Publish' ) ) {
 		return __( 'Save pattern to theme', 'pattern-manager' );
 	}
-	if ( text === __( 'Post published.', 'pattern-manager' ) || text === __( 'Post updated.', 'pattern-manager' ) ) {
+	if ( text === __( 'Post published.' ) || text === __( 'Post updated.' ) ) {
 		return __( 'Pattern saved to theme', 'pattern-manager' );
 	}
-	if ( text === __( 'Update', 'pattern-manager' ) || text === __( 'Post updated.', 'pattern-manager' ) ) {
+	if ( text === __( 'Update' ) || text === __( 'Post updated.' ) ) {
 		return __( 'Update Pattern', 'pattern-manager' );
 	}
-	if ( text === __( 'Add New Tag', 'pattern-manager' ) ) {
+	if ( text === __( 'Add New Tag' ) ) {
 		return __( 'Pattern Categories', 'pattern-manager' );
 	}
-	if ( text === __( 'Saved', 'pattern-manager' ) ) {
+	if ( text === __( 'Saved' ) ) {
 		return __( 'Saved to your theme directory', 'pattern-manager' );
 	}
 
