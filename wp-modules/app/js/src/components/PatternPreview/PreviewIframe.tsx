e475e7a1f3725032b5f4320d5ed542d22832ba0f
Phil Johnston <phil.johnston@wpengine.com>
2023-02-03 16:16:00 -0500

    Make previews the good height

--- wp-modules/app/js/src/components/PatternPreview/PreviewIframe.tsx
+++ wp-modules/app/js/src/components/PatternPreview/PreviewIframe.tsx
@@ -2,9 +2,6 @@
 import { useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
-// Hooks
-import usePmContext from '../../hooks/usePmContext';
-
 // Types
 import type { PreviewIframeProps } from './types';
 
@@ -12,25 +9,56 @@ export default function PreviewIframe( {
 	url,
 	scale,
 	viewportWidth,
-	previewContainerSize,
 }: PreviewIframeProps ) {
+	const [ iframeRef, setIframeRef ] = useState<
+		HTMLIFrameElement | undefined
+	>( undefined );
+	const [ iframeBodyHeight, setIframeBodyHeight ] = useState( 0 );
+	const [ iframeLoaded, setIframeLoaded ] = useState( false );
+
 	return (
-		<iframe
-			src={ url }
-			title={ __( 'Pattern Preview', 'pattern-manager' ) }
-			role="img"
-			style={ {
-				position: 'absolute',
-				top: '0',
-				left: '0',
-				width: viewportWidth,
-				height: previewContainerSize.height / scale,
-				display: 'block',
-				transform: 'scale(' + scale + ')',
-				transformOrigin: 'top left',
-				pointerEvents: 'none',
-			} }
-		/>
+		<>
+			<div
+				className="pattern-preview-iframe-inner"
+				style={ {
+					height: iframeBodyHeight * scale,
+					opacity: iframeLoaded ? 1 : 0,
+				} }
+			>
+				<iframe
+					ref={ setIframeRef }
+					onLoad={ () => {
+						if (
+							iframeRef?.contentWindow?.document?.documentElement
+								?.scrollHeight
+						) {
+							if (
+								iframeRef.contentWindow.document.documentElement
+									.scrollHeight > 0
+							) {
+								setIframeBodyHeight(
+									iframeRef.contentWindow.document
+										.documentElement.scrollHeight
+								);
+							}
+						}
+						setIframeLoaded( true );
+					} }
+					src={ url }
+					title={ __( 'Pattern Preview', 'pattern-manager' ) }
+					role="img"
+					style={ {
+						width: viewportWidth,
+						height: iframeBodyHeight,
+						display: 'block',
+						transform: 'scale(' + scale + ')',
+						transformOrigin: 'top left',
+						pointerEvents: 'none',
+					} }
+				/>
+			</div>
+			<div hidden={ iframeLoaded } className="pattern-loader"></div>
+		</>
 	);
 }
 
