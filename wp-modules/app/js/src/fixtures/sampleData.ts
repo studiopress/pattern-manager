76168b80a67554348127e7aac51d434c7a0726c5
Michael Day <michael.day@wpengine.com>
2023-02-08 12:44:23 -0600

    Create sampleData fixtures

---
+++ wp-modules/app/js/src/fixtures/sampleData.ts
@@ -0,0 +1,31 @@
+import type { Patterns } from '../types';
+
+export const mockPattern: Patterns = {
+	'test-pattern-single': {
+		title: 'Test Pattern Single',
+		name: 'test-pattern-single',
+		slug: 'test-pattern-single',
+		content: '',
+	},
+};
+
+export const mockPatterns: Patterns = {
+	'test-pattern-1': {
+		title: 'Test Pattern 1',
+		name: 'test-pattern-1',
+		slug: 'test-pattern-1',
+		content: '',
+	},
+	'test-pattern-2': {
+		title: 'Test Pattern 2',
+		name: 'test-pattern-2',
+		slug: 'test-pattern-2',
+		content: '',
+	},
+	'test-pattern-3': {
+		title: 'Test Pattern 3',
+		name: 'test-pattern-3',
+		slug: 'test-pattern-3',
+		content: '',
+	},
+};
