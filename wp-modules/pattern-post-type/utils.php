ec2c62144fcd4f5369402962a46f535e689db1ef
Ryan Kienstra <kienstraryan@gmail.com>
2023-02-09 14:48:46 -0600

    Fix a phpcs error from there being no PHP DocBlock

--- wp-modules/pattern-post-type/utils.php
+++ wp-modules/pattern-post-type/utils.php
@@ -10,6 +10,13 @@ declare(strict_types=1);
 
 namespace PatternManager\PatternPostType;
 
+/**
+ * Gets the new pattern number.
+ *
+ * @param string $name The new pattern name.
+ * @param array $all_patterns All of the patterns.
+ * @return int The new pattern number.
+ */
 function get_new_pattern_number( $name, $all_patterns ) {
 	return 0;
 }
