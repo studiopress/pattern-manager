66f08abc2dda0519dc7c242966315e80f9cf8526
Ryan Kienstra <kienstraryan@gmail.com>
2023-02-09 01:23:14 -0600

    Don't go to post.php when it's not needed

--- wp-modules/app/js/src/utils/getAdminUrl.ts
+++ wp-modules/app/js/src/utils/getAdminUrl.ts
@@ -3,7 +3,7 @@ import { patternManager } from '../globals';
 export default function getAdminUrl( queryParams: {
 	[ key: string ]: string;
 } ) {
-	const url = new URL( `${ patternManager.siteUrl }/wp-admin/post-new.php` );
+	const url = new URL( `${ patternManager.siteUrl }/wp-admin/admin.php` );
 	url.searchParams.append( 'post_type', 'pm_pattern' );
 	Object.keys( queryParams ).forEach( ( key ) => {
 		url.searchParams.append( key, queryParams[ key ] );
