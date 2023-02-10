7d573cc9782eccf7ab8d5cd9f4786a2099bf136c
Ryan Kienstra <kienstraryan@gmail.com>
2023-02-10 15:09:57 -0600

    Remove a function that's probably not used anymore

--- wp-modules/pattern-post-type/pattern-post-type.php
+++ wp-modules/pattern-post-type/pattern-post-type.php
@@ -303,20 +303,3 @@ function register_block_patterns() {
 	}
 }
 add_action( 'current_screen', __NAMESPACE__ . '\register_block_patterns', 9 );
-
-/**
- * Modify certain words when editing a pattern.
- *
- * @param string $translation The translated or modified string.
- * @param string $text The original text we'll change.
- * @param string $domain The text domain of the string in question.
- * @return string
- */
-function modify_terms( string $translation, string $text, string $domain ) {
-	if ( 'Tags' === $translation ) {
-		return 'Pattern Categories';
-	}
-
-	return $translation;
-}
-add_filter( 'gettext', __NAMESPACE__ . '\modify_terms', 10, 3 );
