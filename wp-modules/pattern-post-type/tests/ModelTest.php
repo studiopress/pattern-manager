a7c76e6e87d44a7ac5ed4850af192d796c58bcd2
Ryan Kienstra <kienstraryan@gmail.com>
2023-02-20 15:54:09 -0600

    Use stdClass() instead of WP_Post

--- wp-modules/pattern-post-type/tests/ModelTest.php
+++ wp-modules/pattern-post-type/tests/ModelTest.php
@@ -7,6 +7,7 @@
 
 namespace PatternManager\PatternPostType;
 
+use stdClass;
 use WP_UnitTestCase;
 
 require_once dirname( __DIR__ ) . '/model.php';
@@ -20,6 +21,9 @@ class ModelTest extends WP_UnitTestCase {
 	 * Tests ignore_title_field_in_revisions.
 	 */
 	public function test_ignore_title_field_in_revisions_wrong_post() {
+		$post              = new stdClass();
+		$post->post_parent = $this->factory()->post->create( [ 'post_type' => 'page' ] )->ID;
+
 		$this->assertSame(
 			[
 				'post_title'  => 'Example Title',
@@ -31,11 +35,7 @@ class ModelTest extends WP_UnitTestCase {
 					'post_title'  => 'Example Title',
 					'post_author' => 123,
 				],
-				$this->factory()->post->create(
-					[
-						'post_parent' => $this->factory()->post->create( [ 'post_type' => 'page' ] )->ID,
-					]
-				)
+				$post
 			)
 		);
 	}
@@ -54,11 +54,9 @@ class ModelTest extends WP_UnitTestCase {
 					'post_title'  => 'my-new-pattern',
 					'post_author' => 123,
 				],
-				$this->factory()->post->create(
-					[
-						'post_parent' => $this->factory()->post->create( [ 'post_type' => 'pm_pattern' ] )->ID,
-					]
-				)
+				[
+					'post_parent' => $this->factory()->post->create( [ 'post_type' => 'pm_pattern' ] )->ID,
+				]
 			)
 		);
 	}
@@ -67,6 +65,8 @@ class ModelTest extends WP_UnitTestCase {
 	 * Tests ignore_title_field_in_revisions.
 	 */
 	public function test_ignore_title_field_in_revisions_correct_post() {
+		$post              = new stdClass();
+		$post->post_parent = $this->factory()->post->create( [ 'post_type' => 'pm_pattern' ] )->ID;
 		$this->assertSame(
 			[ 'post_author' => 123 ],
 			ignore_title_field_in_revisions(
@@ -74,11 +74,7 @@ class ModelTest extends WP_UnitTestCase {
 					'post_title'  => 'my-new-pattern',
 					'post_author' => 123,
 				],
-				$this->factory()->post->create(
-					[
-						'post_parent' => $this->factory()->post->create( [ 'post_type' => 'pm_pattern' ] )->ID,
-					]
-				)
+				$post
 			)
 		);
 	}
