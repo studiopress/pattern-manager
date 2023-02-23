d497807508c8e082731c2b951e445056ac5c8b62
Ryan Kienstra <kienstraryan@gmail.com>
2023-02-23 15:33:24 -0600

    Test get_pattern_by_path() instead

--- wp-modules/pattern-data-handlers/tests/PatternDataHandlersTest.php
+++ wp-modules/pattern-data-handlers/tests/PatternDataHandlersTest.php
@@ -17,15 +17,31 @@ require_once dirname( __DIR__ ) . '/pattern-data-handlers.php';
  */
 class PatternDataHandlersTest extends WP_UnitTestCase {
 
-	/**
-	 * Tests add_active_theme_to_heartbeat.
-	 */
-	public function test_update_pattern_empty() {
-		update_pattern( [ 'name' => 'foo' ] );
+	public function normalize( $to_normalize ) {
+		return preg_replace( '/[\t\n]/', '', $to_normalize );
+	}
 
+	/**
+	 * Tests get_pattern_by_path.
+	 */
+	public function test_get_pattern_by_path() {
 		$this->assertSame(
-			[ 'name' => 'foo' ],
-			get_pattern_by_name( 'foo' )
+			[
+				'name'          => 'my-new-pattern',
+				'title'         => 'My New Pattern',
+				'slug'          => 'my-new-pattern',
+				'description'   => 'Here is a description',
+				'viewportWidth' => '1280',
+				'categories'    => [ 'contact', 'featured' ],
+				'keywords'      => [ 'example', 'music' ],
+				'blockTypes'    => [ 'core/gallery', 'core/media-text' ],
+				'postTypes'     => [ 'wp_block', 'wp_template' ],
+				'inserter'      => true,
+				'content'       => '<!-- wp:paragraph -->
+				<p>Here is some content</p>
+				<!-- /wp:paragraph -->'
+			],
+			get_pattern_by_path( __DIR__ . '/fixtures/my-new-pattern.php' )
 		);
 	}
 }
