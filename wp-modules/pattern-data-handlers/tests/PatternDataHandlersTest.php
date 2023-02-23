613acedb5e89ccf38a046b42860287142e397bc0
Ryan Kienstra <kienstraryan@gmail.com>
2023-02-23 15:43:13 -0600

    Add a test for construct_pattern_php_file_contents()

--- wp-modules/pattern-data-handlers/tests/PatternDataHandlersTest.php
+++ wp-modules/pattern-data-handlers/tests/PatternDataHandlersTest.php
@@ -17,7 +17,10 @@ require_once dirname( __DIR__ ) . '/pattern-data-handlers.php';
  */
 class PatternDataHandlersTest extends WP_UnitTestCase {
 
-	public function normalize( $to_normalize ) {
+	/**
+	 * Normalizes in order to compare in tests.
+	 */
+	public function normalize( string $to_normalize ): string {
 		return preg_replace( '/[\t\n]/', '', $to_normalize );
 	}
 
@@ -42,6 +45,25 @@ class PatternDataHandlersTest extends WP_UnitTestCase {
 				<!-- /wp:paragraph -->',
 			],
 			get_pattern_by_path( __DIR__ . '/fixtures/my-new-pattern.php' )
+		);
+	}
+
+	/**
+	 * Tests construct_pattern_php_file_contents.
+	 */
+	public function test_construct_pattern_php_file_contents() {
+		$pattern_path = __DIR__ . '/fixtures/my-new-pattern.php';
+
+		ob_start();
+		require $pattern_path;
+		$pattern_contents = ob_get_clean();
+
+		$this->assertSame(
+			$pattern_contents,
+			construct_pattern_php_file_contents(
+				get_pattern_by_path( $pattern_path ),
+				'foo-textdomain'
+			)
 		);
 	}
 }
