fe5a8976d0a67f8775308b309621323c9a7330a7
Ryan Kienstra <kienstraryan@gmail.com>
2023-02-27 00:00:53 -0600

    Remove unit tests for deleted function

--- wp-modules/editor/tests/ModelTest.php
+++ wp-modules/editor/tests/ModelTest.php
@@ -48,69 +48,6 @@ class ModelTest extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Tests ignore_title_field_in_revisions.
-	 */
-	public function test_ignore_title_field_in_revisions_wrong_post() {
-		$post              = new stdClass();
-		$post->post_parent = $this->factory()->post->create( [ 'post_type' => 'page' ] );
-
-		$this->assertSame(
-			[
-				'post_title'  => 'Example Title',
-				'post_author' => 123,
-
-			],
-			ignore_title_field_in_revisions(
-				[
-					'post_title'  => 'Example Title',
-					'post_author' => 123,
-				],
-				$post
-			)
-		);
-	}
-
-	/**
-	 * Tests ignore_title_field_in_revisions.
-	 */
-	public function test_ignore_title_field_in_revisions_array_post() {
-		$this->assertSame(
-			[
-				'post_title'  => 'my-new-pattern',
-				'post_author' => 123,
-			],
-			ignore_title_field_in_revisions(
-				[
-					'post_title'  => 'my-new-pattern',
-					'post_author' => 123,
-				],
-				[
-					'post_parent' => $this->factory()->post->create( [ 'post_type' => 'pm_pattern' ] ),
-				]
-			)
-		);
-	}
-
-	/**
-	 * Tests ignore_title_field_in_revisions.
-	 */
-	public function test_ignore_title_field_in_revisions_correct_post() {
-		$post              = new stdClass();
-		$post->post_parent = $this->factory()->post->create( [ 'post_type' => 'pm_pattern' ] );
-
-		$this->assertSame(
-			[ 'post_author' => 123 ],
-			ignore_title_field_in_revisions(
-				[
-					'post_title'  => 'my-new-pattern',
-					'post_author' => 123,
-				],
-				$post
-			)
-		);
-	}
-
-	/**
 	 * Tests delete_pattern_posts.
 	 */
 	public function test_delete_pattern_posts_wrong_post() {
