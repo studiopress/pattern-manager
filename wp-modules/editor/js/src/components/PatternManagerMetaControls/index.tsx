18033031cdc0da0fba0d7f1427f9215ac5f2a8c5
Ryan Kienstra <kienstraryan@gmail.com>
2023-02-28 14:25:20 -0600

    Make the title prop be in alphabetical order

--- wp-modules/editor/js/src/components/PatternManagerMetaControls/index.tsx
+++ wp-modules/editor/js/src/components/PatternManagerMetaControls/index.tsx
@@ -31,9 +31,9 @@ export default function PatternManagerMetaControls() {
 		<div>
 			<TitlePanel
 				postMeta={ postMeta }
-				title={ title }
 				handleChange={ updatePostMeta }
 				patternNames={ patternNames }
+				title={ title }
 			/>
 			<CategoriesPanel
 				postMeta={ postMeta }
