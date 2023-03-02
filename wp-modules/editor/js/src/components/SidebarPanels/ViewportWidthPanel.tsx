eedf868d2511a7f3e4bdbb63e5f8ad974cb37772
Michael Day <michael.day@wpengine.com>
2023-03-02 13:27:09 -0600

    Modify text from code review suggestion

--- wp-modules/editor/js/src/components/SidebarPanels/ViewportWidthPanel.tsx
+++ wp-modules/editor/js/src/components/SidebarPanels/ViewportWidthPanel.tsx
@@ -30,7 +30,7 @@ export default function ViewportWidthPanel( {
 				label={ __( 'Preview width in pixels', 'pattern-manager' ) }
 				hideLabelFromVision={ true }
 				help={ __(
-					'Adjust the width of the pattern preview in the pattern inserter.',
+					'Adjust the pattern preview width in the pattern inserter.',
 					'pattern-manager'
 				) }
 				min={ 640 }
