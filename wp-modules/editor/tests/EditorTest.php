10162a5aaf7a06fc007904331b8abbb361b12073
Ryan Kienstra <kienstraryan@gmail.com>
2023-03-03 17:32:13 -0600

    Will revert: cause a PHPUnit error

--- wp-modules/editor/tests/EditorTest.php
+++ wp-modules/editor/tests/EditorTest.php
@@ -42,7 +42,7 @@ class EditorTest extends WP_UnitTestCase {
 
 		foreach ( array_diff( get_pattern_defaults(), [ 'title' => null ] ) as $meta_key => $default_value ) {
 			$this->assertSame(
-				$default_value,
+				'something',
 				get_registered_meta_keys( 'post', 'pm_pattern' )[ $meta_key ]['default'],
 				"default type of {$meta_key} is wrong"
 			);
