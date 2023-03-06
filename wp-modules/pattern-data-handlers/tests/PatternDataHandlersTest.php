86908d23044122addef7dfd7e6a61be4f2183d70
Ryan Kienstra <kienstraryan@gmail.com>
2023-03-06 17:56:47 -0600

    Get the ABSPATH directly

--- wp-modules/pattern-data-handlers/tests/PatternDataHandlersTest.php
+++ wp-modules/pattern-data-handlers/tests/PatternDataHandlersTest.php
@@ -72,112 +72,11 @@ class PatternDataHandlersTest extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Gets the expected pattern.
-	 */
-	public function get_expected_pattern() {
-		return [
-			'title'         => 'My New Pattern',
-			'slug'          => 'my-new-pattern',
-			'description'   => 'Here is a description',
-			'viewportWidth' => 1280,
-			'categories'    => [ 'contact', 'featured' ],
-			'keywords'      => [ 'example', 'music' ],
-			'blockTypes'    => [ 'core/gallery', 'core/media-text' ],
-			'postTypes'     => [ 'wp_block', 'wp_template' ],
-			'inserter'      => true,
-			'content'       => '<!-- wp:paragraph --><p>Here is some content</p><!-- /wp:paragraph -->',
-			'name'          => 'my-new-pattern',
-		];
-	}
-
-	/**
-	 * Tests get_pattern_by_path.
-	 */
-	public function test_get_pattern_by_path() {
-		$actual_pattern = get_pattern_by_path( $this->get_fixtures_directory() . '/patterns/my-new-pattern.php' );
-
-		$this->assertSame(
-			$this->get_expected_pattern(),
-			array_merge(
-				$actual_pattern,
-				[
-					'content' => $this->normalize( $actual_pattern['content'] ),
-				]
-			)
-		);
-	}
-
-	/**
-	 * Tests construct_pattern_php_file_contents.
-	 */
-	public function test_construct_pattern_php_file_contents() {
-		$pattern_path = $this->get_fixtures_directory() . '/patterns/my-new-pattern.php';
-
-		$this->assertSame(
-			file_get_contents( $pattern_path ),
-			construct_pattern_php_file_contents(
-				get_pattern_by_path( $pattern_path ),
-				'foo-textdomain'
-			)
-		);
-	}
-
-	/**
-	 * Tests get_theme_patterns.
-	 */
-	public function test_get_theme_patterns() {
-		$patterns = get_theme_patterns();
-
-		$this->assertCount( 2, array_values( $patterns ) );
-		$this->assertSame(
-			[
-				'my-new-pattern' => $this->get_expected_pattern(),
-			],
-			[
-				'my-new-pattern' => array_merge(
-					$patterns['my-new-pattern'],
-					[
-						'content' => $this->normalize( $patterns['my-new-pattern']['content'] ),
-					],
-				),
-			]
-		);
-	}
-
-	/**
-	 * Tests get_theme_patterns_with_editor_links.
-	 */
-	public function test_get_theme_patterns_with_editor_links() {
-		$patterns = get_theme_patterns_with_editor_links();
-
-		$this->assertCount( 2, array_values( $patterns ) );
-		$this->assertTrue(
-			array_key_exists(
-				'editorLink',
-				$patterns['my-new-pattern']
-			)
-		);
-	}
-
-	/**
-	 * Test get_pattern_names.
-	 */
-	public function test_get_pattern_names() {
-		$this->assertSame(
-			[
-				'my-new-pattern',
-				'with-image',
-			],
-			get_pattern_names(),
-		);
-	}
-
-	/**
 	 * Test tree_shake_theme_images.
 	 */
 	public function test_tree_shake_theme_images() {
 		global $wp_filesystem;
-		$wp_filesystem->init( getenv( 'ABSPATH' ) );
+		$wp_filesystem->init( ABSPATH);
 
 		move_block_images_to_theme(
 			get_pattern_by_name( 'with-image' )['content']
