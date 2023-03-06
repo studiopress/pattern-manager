4724ad5d27613efb98a24fe4b11a33d835cbc6a4
Ryan Kienstra <kienstraryan@gmail.com>
2023-03-06 16:43:27 -0600

    Add a missing ;

--- wp-modules/pattern-data-handlers/tests/PatternDataHandlersTest.php
+++ wp-modules/pattern-data-handlers/tests/PatternDataHandlersTest.php
@@ -53,7 +53,7 @@ class PatternDataHandlersTest extends WP_UnitTestCase {
 	 * Filters the abstraction file.
 	 */
 	public function filter_abstraction_file( $file ) {
-		return WP_TESTS_DIR . '/includes/mock-fs.php'
+		return WP_TESTS_DIR . '/includes/mock-fs.php';
 	}
 
 	/**
