78387301df9591fbbd1d24cb402ac99ae63a1f50
Ryan Kienstra <kienstraryan@gmail.com>
2023-03-06 23:26:37 -0600

    Add a filter back in, which was needed

--- wp-modules/pattern-data-handlers/tests/PatternDataHandlersTest.php
+++ wp-modules/pattern-data-handlers/tests/PatternDataHandlersTest.php
@@ -24,6 +24,7 @@ class PatternDataHandlersTest extends WP_UnitTestCase {
 	 */
 	public function setUp() {
 		parent::setUp();
+		add_filter( 'request_filesystem_credentials', '__return_true' );
 		add_filter( 'stylesheet_directory', [ $this, 'get_fixtures_directory' ] );
 	}
 
@@ -31,6 +32,7 @@ class PatternDataHandlersTest extends WP_UnitTestCase {
 	 * @inheritDoc
 	 */
 	public function tearDown() {
+		remove_filter( 'request_filesystem_credentials', '__return_true' );
 		remove_filter( 'stylesheet_directory', [ $this, 'get_fixtures_directory' ] );
 		parent::tearDown();
 	}
