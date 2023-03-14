1ea37f7b1a6d755147e154f387c598e84eff1e62
Michael Day <michael.day@wpengine.com>
2023-03-14 14:57:24 -0500

    Create new useForceUpdate hook

---
+++ wp-modules/app/js/src/hooks/useForceUpdate.ts
@@ -0,0 +1,33 @@
+import { useLayoutEffect, useState, useRef } from '@wordpress/element';
+
+type WindowDimensions = [ number, number ];
+
+/**
+ * Re-render the calling component when the window is resized.
+ *
+ * Optionally include an array of dependencies to trigger a re-render.
+ */
+export default function useForceUpdate< T extends unknown >( dependencies: T[] = [] ) {
+	const [ , setForceUpdate ] = useState< T[] | WindowDimensions >();
+	const initialRender = useRef( true );
+
+	useLayoutEffect( () => {
+		// Do not cause a re-render on first render.
+		if ( initialRender.current ) {
+			initialRender.current = false;
+		} else {
+			setForceUpdate( dependencies );
+		}
+
+		function updateSizeAndRerender() {
+			setForceUpdate( [
+				window.innerWidth,
+				window.innerHeight,
+			] as WindowDimensions );
+		}
+
+		window.addEventListener( 'resize', updateSizeAndRerender );
+		return () =>
+			window.removeEventListener( 'resize', updateSizeAndRerender );
+	}, [ ...dependencies ] );
+}
