a44aefd01b151647d7828816797ad29d1149e844
Ryan Kienstra <kienstraryan@gmail.com>
2023-03-22 14:46:49 -0500

    Add a test file for api-data/

---
+++ wp-modules/api-data/tests/ApiDataTest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Class ApiDataTest
+ *
+ * @package pattern-manager
+ */
+
+namespace PatternManager\ApiData;
+
+use WP_UnitTestCase;
+
+require_once dirname( __DIR__ ) . '/api-data.php';
+
+/**
+ * Test the api functions.
+ */
+class ApiDataTest extends WP_UnitTestCase {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function setUp() {
+		parent::setUp();
+		add_filter( 'request_filesystem_credentials', '__return_true' );
+		add_filter( 'stylesheet_directory', [ $this, 'get_fixtures_directory' ] );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function tearDown() {
+		remove_filter( 'request_filesystem_credentials', '__return_true' );
+		remove_filter( 'stylesheet_directory', [ $this, 'get_fixtures_directory' ] );
+		parent::tearDown();
+	}
+
+	/**
+	 * Gets the fixtures directory.
+	 */
+	public function get_fixtures_directory() {
+		return dirname( dirname( __DIR__ ) ) . '/pattern-data-handlers/tests/fixtures';
+	}
+
+	/**
+	 * Tests register_routes.
+	 */
+	public function test_register_routes() {
+		$request  = new WP_REST_Request( 'GET', '/wp/v2/block-patterns/categories' );
+		$response = rest_do_request( $request );
+
+		$this->assertEquals(
+			[
+				'my-new-pattern',
+				'with-image',
+			],
+			rest_get_server()->response_to_data(
+				rest_do_request( new WP_REST_Request( 'GET', 'pattern-manager/v1/get-pattern-names' ) )
+			)
+		);
+	}
+}
