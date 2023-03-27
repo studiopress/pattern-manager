090b7d7207d267aa3234d74621a6c3c9cc33185e
Michael Day <michael.day@wpengine.com>
2023-03-27 16:44:11 -0500

    Use customCategories in categoriesPanel; create initial, messy logic for parsing missing categories

--- wp-modules/editor/js/src/components/SidebarPanels/CategoriesPanel.tsx
+++ wp-modules/editor/js/src/components/SidebarPanels/CategoriesPanel.tsx
@@ -1,7 +1,8 @@
 import { __ } from '@wordpress/i18n';
 import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
 import { Spinner } from '@wordpress/components';
-import Select from 'react-select';
+import Creatable from 'react-select/creatable';
+import convertToSlug from '../../utils/convertToSlug';
 
 import type { BaseSidebarProps, AdditionalSidebarProps } from './types';
 
@@ -12,46 +13,111 @@ import type { BaseSidebarProps, AdditionalSidebarProps } from './types';
 export default function CategoriesPanel( {
 	categories,
 	categoryOptions,
+	customCategories,
 	handleChange,
-}: BaseSidebarProps< 'categories' > &
+}: BaseSidebarProps< 'categories' | 'customCategories' > &
 	AdditionalSidebarProps< 'categoryOptions' > ) {
+	// The list of currently selected categories, formatted for react-select.
+	const selectedCategories: typeof categoryOptions = categories.reduce(
+		( acc, categoryName ) =>
+			! acc.includes( categoryName )
+				? [
+						...acc,
+						[ ...categoryOptions, ...getMissingPmCategories() ].find(
+							( matchedCategory ) =>
+								matchedCategory.value === categoryName
+						),
+				  ]
+				: acc,
+		[]
+	);
+
+	function getMissingPmCategories() {
+		const categoryLabels = categoryOptions?.map(
+			( category ) => category.label
+		);
+
+		const missingCategoryLabels = customCategories.filter(
+			( customCategoryLabel ) =>
+				categoryLabels.length &&
+				! categoryLabels.includes( customCategoryLabel )
+		);
+
+		return missingCategoryLabels.map( ( missingLabel ) => ( {
+			label: missingLabel,
+			value: convertToSlug( missingLabel ),
+		} ) );
+	};
+
 	return (
 		<PluginDocumentSettingPanel
 			name="patternmanager-pattern-editor-pattern-categories"
 			title={ __( 'Pattern Categories', 'pattern-manager' ) }
 		>
 			{ categoryOptions ? (
-				<Select
-					isMulti
-					isClearable
-					closeMenuOnSelect={ false }
-					aria-label={ __(
-						'Add Pattern Categories',
-						'pattern-manager'
-					) }
-					value={ categories?.map( ( category ) =>
-						categoryOptions.find(
-							( matchedCategory ) =>
-								matchedCategory.value === category
-						)
-					) }
-					options={ categoryOptions }
-					onChange={ ( categorySelections ) => {
-						handleChange(
-							'categories',
-							categorySelections.map(
+				<>
+					<Creatable
+						isMulti
+						isClearable
+						closeMenuOnSelect={ false }
+						aria-label={ __(
+							'Add Pattern Categories',
+							'pattern-manager'
+						) }
+						value={ selectedCategories }
+						options={ categoryOptions }
+						onChange={ ( categorySelections ) => {
+							const selections = categorySelections.map(
 								( category ) => category.value
-							)
-						);
-					} }
-					menuPlacement="auto"
-					styles={ {
-						menu: ( base ) => ( {
-							...base,
-							zIndex: 100,
-						} ),
-					} }
-				/>
+							);
+
+							const customCategorySelections =
+								categorySelections.reduce(
+									( acc, { label } ) =>
+										customCategories.includes( label )
+											? [ ...acc, label ]
+											: acc,
+									[]
+								);
+
+							handleChange( 'categories', selections, {
+								customCategories: customCategorySelections,
+							} );
+						} }
+						onCreateOption={ ( newCategoryTitle ) => {
+							handleChange(
+								'categories',
+								[
+									...categories,
+									convertToSlug( newCategoryTitle ),
+								],
+								{
+									customCategories: [
+										...customCategories,
+										newCategoryTitle,
+									],
+								}
+							);
+						} }
+						menuPlacement="auto"
+						styles={ {
+							menu: ( base ) => ( {
+								...base,
+								zIndex: 100,
+							} ),
+						} }
+					/>
+
+					{ getMissingPmCategories().length
+						? getMissingPmCategories().map( ( missingCategory ) => {
+								return (
+									<div>
+										Missing: { missingCategory.label }
+									</div>
+								);
+						  } )
+						: null }
+				</>
 			) : (
 				<Spinner />
 			) }
