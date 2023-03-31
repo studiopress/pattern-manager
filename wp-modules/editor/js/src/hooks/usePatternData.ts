a69ed4d3b9f9cab444e924d3599be949259f61af
Michael Day <michael.day@wpengine.com>
2023-03-31 13:38:09 -0500

    Add the core/query blockType

--- wp-modules/editor/js/src/hooks/usePatternData.ts
+++ wp-modules/editor/js/src/hooks/usePatternData.ts
@@ -87,6 +87,11 @@ export default function usePatternData( postMeta: PostMeta ) {
 					} ),
 				} ) ),
 			{
+				label: 'core/query',
+				value: 'core/query',
+				transforms: {},
+			},
+			{
 				label: 'core/template-part/header',
 				value: 'core/template-part/header',
 				transforms: {},
