678eab330361016e538490749cf29c75c89ee8b1
Ryan Kienstra <kienstraryan@gmail.com>
2023-04-10 15:46:25 -0600

    Add a test case for innerBlocks

--- wp-modules/editor/js/src/utils/test/convertBlocksToTemplate.ts
+++ wp-modules/editor/js/src/utils/test/convertBlocksToTemplate.ts
@@ -4,12 +4,31 @@
 import convertBlocksToTemplate from '../convertBlocksToTemplate';
 
 describe( 'convertBlocksToTemplate', () => {
-	it.each( [ [ [], [] ] ] )(
-		'should convert blocks',
-		( blocks, expected ) => {
-			expect( convertParsedBlocksToBlockTemplate( blocks ) ).toEqual(
-				expected
-			);
-		}
-	);
+	it.each( [
+		[ [], [] ],
+		[
+			[ { name: 'Foo', attributes: { bar: '' } } ],
+			[ [ 'Foo', { bar: '' }, undefined ] ],
+		],
+		[
+			[
+				{
+					name: 'Foo',
+					attributes: { bar: '' },
+					innerBlocks: [
+						{ name: 'Bar', attributes: { example: '' } },
+					],
+				},
+			],
+			[
+				[
+					'Foo',
+					{ bar: '' },
+					[ [ 'Bar', { example: '' }, undefined ] ],
+				],
+			],
+		],
+	] )( 'should convert blocks', ( blocks, expected ) => {
+		expect( convertBlocksToTemplate( blocks ) ).toEqual( expected );
+	} );
 } );
