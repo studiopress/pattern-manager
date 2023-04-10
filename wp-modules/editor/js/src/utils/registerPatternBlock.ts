1eddedc926f2605b0348ecca48d0957b9e97c45d
Ryan Kienstra <kienstraryan@gmail.com>
2023-04-10 15:29:06 -0600

    Add file I forgot to stage

---
+++ wp-modules/editor/js/src/utils/registerPatternBlock.ts
@@ -0,0 +1,27 @@
+import { __ } from '@wordpress/i18n';
+import PatternEdit from '../components/PatternEdit';
+
+export default function registerPatternBlock(
+	settings: Record< string, unknown >,
+	name: string
+) {
+	return name === 'core/pattern'
+		? {
+				...settings,
+				apiVersion: 1,
+				title: __( 'PM Pattern Block', 'pattern-manager' ),
+				icon: '',
+				category: 'common',
+				attributes: {
+					slug: {
+						type: 'string',
+					},
+				},
+				supports: {
+					inserter: true,
+				},
+				edit: PatternEdit,
+				save: () => null,
+		  }
+		: settings;
+}
