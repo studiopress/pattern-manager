6b491be977bddc43d4cc7b6b8258c75382db722d
Michael Day <michael.day@wpengine.com>
2023-04-11 15:58:08 -0500

    Delete unused utils from app and editor

--- wp-modules/editor/js/src/utils/getHeaders.ts
+++
@@ -1,9 +0,0 @@
-import { patternManager } from '../globals';
-
-export default function getHeaders() {
-	return {
-		Accept: 'application/json',
-		'Content-Type': 'application/json',
-		'X-WP-Nonce': patternManager.apiNonce,
-	};
-}
