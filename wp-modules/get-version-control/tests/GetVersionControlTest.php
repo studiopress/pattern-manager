3e22e49f621625b8aa47c2d51612515e89f4cfb3
Michael Day <michael.day@wpengine.com>
2023-04-20 16:18:40 -0500

    Simplify the phpunit tests, delete unneeded fixture theme files

--- wp-modules/get-version-control/tests/GetVersionControlTest.php
+++ wp-modules/get-version-control/tests/GetVersionControlTest.php
@@ -10,10 +10,9 @@ namespace PatternManager\GetVersionControl;
 use WP_UnitTestCase;
 
 require_once dirname( __DIR__ ) . '/get-version-control.php';
-require_once GetVersionControlTest::get_mock_theme_directory() . '/functions.php';
 
 /**
- * Test the app.
+ * Test the `get-version-control` module.
  */
 class GetVersionControlTest extends WP_UnitTestCase {
 	/**
@@ -21,52 +20,42 @@ class GetVersionControlTest extends WP_UnitTestCase {
 	 */
 	public function setUp() {
 		parent::setUp();
-		add_filter( 'request_filesystem_credentials', '__return_true' );
-		add_filter( 'muplugins_loaded', [ $this, 'switch_to_test_theme' ] );
+		add_filter( 'template_directory', [ $this, 'get_template_directory' ] );
 	}
 
 	/**
 	 * @inheritDoc
 	 */
 	public function tearDown() {
-		remove_filter( 'request_filesystem_credentials', '__return_true' );
-		remove_filter( 'muplugins_loaded', [ $this, 'switch_to_test_theme' ] );
+		remove_filter( 'template_directory', [ $this, 'get_template_directory' ] );
 		parent::tearDown();
 	}
 
 	/**
-	 * Static method to get the mock theme directory.
+	 * Get the mock theme directory.
 	 */
-	public static function get_mock_theme_directory() {
-		return __DIR__ . '/fixtures/themes/test-theme';
+	public function get_mock_theme_directory() {
+		return __DIR__ . '/fixtures/themes/mock-theme';
 	}
 
 	/**
-	 * Switches to test-theme while running tests in this class.
+	 * Override `get_template_directory()` while running tests in this class.
 	 */
-	public function switch_to_test_theme() {
-		register_theme_directory( __DIR__ . '/fixtures/themes' );
-		switch_theme( 'test-theme' );
+	public function get_template_directory() {
+		return $this->get_mock_theme_directory();
 	}
 
 	/**
-	 * Check that the test theme is active.
-	 */
-	public function test_test_theme_is_active() {
-		$this->assertTrue( 'test-theme' == wp_get_theme(), 'Test setup failed to switch theme to test-theme.' );
-	}
-
-	/**
-	 * Gets the mock theme directory.
+	 * Tests that the `test-theme` fixture was properly setup.
 	 */
 	public function test_test_theme_contains_git_folder() {
-		$this->assertDirectoryExists( self::get_mock_theme_directory() . '/.git', 'Git directory is missing from test-theme fixture.' );
+		$this->assertDirectoryExists( $this->get_mock_theme_directory() . '/.git', 'Git directory is missing from test-theme fixture.' );
 	}
 
 	/**
-	 * Tests check_for_git_in_theme.
+	 * Tests `check_for_git_in_theme()` from the `get-version-control` module.
 	 */
 	public function test_check_for_git_in_theme() {
-		$this->assertTrue( (boolean) check_for_git_in_theme(), 'Failed to find git directory in test-theme.' );
+		$this->assertTrue( (bool) check_for_git_in_theme() );
 	}
 }
---
+++ wp-modules/get-version-control/tests/fixtures/themes/mock-theme/index.php
@@ -0,0 +1,3 @@
+<?php
+// The theme is never initialized, so we don't need to register a stylesheet, etc.
+// A hidden .git folder should be present in this directory.
