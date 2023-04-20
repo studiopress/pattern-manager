d3f6d6b11c597af9fdaad8a63ed38dd357d93352
Michael Day <michael.day@wpengine.com>
2023-04-20 17:49:09 -0500

    Make the notice dismissible; adjust notice style to go full-width

--- wp-modules/app/js/src/components/VersionControlNotice/index.tsx
+++ wp-modules/app/js/src/components/VersionControlNotice/index.tsx
@@ -1,22 +1,24 @@
 // WP dependencies
 import { Notice } from '@wordpress/components';
+import { useState } from '@wordpress/element';
 
 // Globals
 import { patternManager } from '../../globals';
 
 /** Display a non-dismissible notice if no version control is detected in the theme. */
 export default function VersionControlNotice() {
-	return (
-		! patternManager.versionControl && (
-			<Notice
-				className="patternmanager-version-control-notice"
-				isDismissible={ false }
-				status="error"
-			>
-				No version control detected for this theme. Please{ ' ' }
-				<a href="https://github.com/git-guides">initialize git</a> in
-				the <span>{ patternManager.themeName }</span> theme folder.
-			</Notice>
-		)
-	);
+	const [ isVisible, setIsVisible ] = useState( true );
+
+	return ! patternManager.versionControl && isVisible ? (
+		<Notice
+			className="patternmanager-version-control-notice"
+			isDismissible
+			status="error"
+			onRemove={ () => setIsVisible( false ) }
+		>
+			No version control detected for this theme. Please{ ' ' }
+			<a href="https://github.com/git-guides">initialize git</a> in the{ ' ' }
+			<span>{ patternManager.themeName }</span> theme folder.
+		</Notice>
+	) : null;
 }
