a8dbda1f8ea0c5e9f91b8df6d799bd1c049b5134
Michael Day <michael.day@wpengine.com>
2023-04-19 20:44:14 -0500

    Remove unused check for git in theme root

--- wp-modules/get-version-control/get-version-control.php
+++ wp-modules/get-version-control/get-version-control.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Module Name: Get Version Control
- * Description: Checks the current theme or wp-content/themes directory for version control.
+ * Description: Checks the current theme directory for version control.
  * Namespace: GetVersionControl
  *
  * @package pattern-manager
@@ -19,14 +19,4 @@ namespace PatternManager\GetVersionControl;
 function check_for_git_in_theme() {
 	$theme_git_dir = get_template_directory() . '/.git';
 	return file_exists( $theme_git_dir );
-}
-
-/**
- * Checks for a .git folder in the root themes directory.
- *
- * @return boolean
- */
-function check_for_git_in_themes_root() {
-	$themes_root_git_dir = get_theme_root() . '/.git';
-	return file_exists( $themes_root_git_dir ) ? true : false;
 }
