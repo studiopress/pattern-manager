36641de6a20c2f51f91e43a97b989632943bbb20
Michael Day <michael.day@wpengine.com>
2023-04-26 23:46:23 -0500

    Add a new `useVersionControl` hook

---
+++ wp-modules/app/js/src/hooks/useVersionControl.ts
@@ -0,0 +1,52 @@
+import { useState } from '@wordpress/element';
+import { patternManager } from '../globals';
+import getHeaders from '../utils/getHeaders';
+import type { Theme } from '../types';
+
+export default function useVersionControl(
+	initialDismissedThemes: Array< Theme[ 'name' ] >
+) {
+	const [ dismissedThemes, setDismissedThemes ] = useState<
+		Array< Theme[ 'name' ] >
+	>( initialDismissedThemes || [] );
+
+	const warningShouldShow =
+		! Boolean( patternManager.versionControl ) &&
+		! dismissedThemes.some(
+			( dismissedTheme ) => dismissedTheme === patternManager.themeName
+		);
+
+	function updateDismissedThemes( themeNameToDismiss: Theme[ 'name' ] ) {
+		return fetch(
+			patternManager.apiEndpoints.updateDismissedThemesEndpoint,
+			{
+				method: 'POST',
+				headers: getHeaders(),
+				body: JSON.stringify( {
+					themeNames: dismissedThemes.reduce(
+						( acc, themeName ) => {
+							return ! acc.includes( themeName )
+								? [ ...acc, themeName ]
+								: acc;
+						},
+						[ themeNameToDismiss ]
+					),
+				} ),
+			}
+		)
+			.then( ( response ) => response.json() )
+			.then(
+				( data: {
+					message: string;
+					dismissedThemes: Array< Theme[ 'name' ] >;
+				} ) => {
+					setDismissedThemes( data.dismissedThemes );
+				}
+			);
+	}
+
+	return {
+		warningShouldShow,
+		updateDismissedThemes,
+	};
+}
