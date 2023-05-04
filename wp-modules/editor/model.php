12a3de2d506d9b2517749bc9d45522ae78902d70
Ryan Kienstra <kienstraryan@gmail.com>
2023-05-04 11:20:26 -0600

    Revert "Allow saving post meta when the slug doesn't match the title"
    This reverts commit de8ee7f9288df9511571c06d2d4ad95b81914470.

--- wp-modules/editor/model.php
+++ wp-modules/editor/model.php
@@ -122,21 +122,15 @@ function save_metadata_to_pattern_file( $override, $post_id, $meta_key, $meta_va
 	}
 
 	return update_pattern(
-		$pattern
-			? array_merge(
-				$pattern,
-				[
-					$meta_key => $meta_value,
-				]
-			)
-			: array_merge(
-				get_pattern_defaults(),
-				[
-					'name'  => $pattern_name,
-					'title' => $post->post_title,
-				],
-				[ $meta_key => $meta_value ]
-			)
+		array_merge(
+			get_pattern_defaults(),
+			$pattern ? $pattern : [],
+			[
+				'name'  => $pattern_name,
+				'title' => $post->post_title,
+			],
+			[ $meta_key => $meta_value ]
+		)
 	);
 }
 add_filter( 'update_post_metadata', __NAMESPACE__ . '\save_metadata_to_pattern_file', 10, 4 );
