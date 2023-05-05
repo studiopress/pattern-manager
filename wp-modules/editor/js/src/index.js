d11cc30c089980873c1c027dec5e8cb2342acda4
Ryan Kienstra <kienstraryan@gmail.com>
2023-05-05 01:03:30 -0600

    Bring back Patternception for the next release

--- wp-modules/editor/js/src/index.js
+++ wp-modules/editor/js/src/index.js
@@ -5,6 +5,8 @@ import { registerPlugin } from '@wordpress/plugins';
 import BackButton from './components/BackButton';
 import PatternManagerMetaControls from './components/PatternManagerMetaControls';
 import changeWords from './utils/changeWords';
+import preventTransform from './utils/preventTransform';
+import registerPatternBlock from './utils/registerPatternBlock';
 import receiveActiveTheme from './utils/receiveActiveTheme';
 
 registerPlugin( 'pattern-manager-postmeta-for-patterns', {
@@ -18,6 +20,16 @@ registerPlugin( 'pattern-manager-back-button', {
 } );
 
 addFilter( 'i18n.gettext', 'pattern-manager/changeWords', changeWords );
+addFilter(
+	'blocks.registerBlockType',
+	'pattern-manager/preventTransform',
+	preventTransform
+);
+addFilter(
+	'blocks.registerBlockType',
+	'pattern-manager/registerPatternBlock',
+	registerPatternBlock
+);
 addAction(
 	'heartbeat.tick',
 	'pattern-manager/checkActiveTheme',
