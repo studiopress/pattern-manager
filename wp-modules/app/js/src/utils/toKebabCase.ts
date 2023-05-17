cfb3814060e8f5a769e7b394cf7b140a48dc4b29
Ryan Kienstra <kienstraryan@gmail.com>
2023-05-17 15:41:12 -0600

    Remove an extra 'a'

--- wp-modules/app/js/src/utils/toKebabCase.ts
+++ wp-modules/app/js/src/utils/toKebabCase.ts
@@ -1,4 +1,4 @@
-/** Converts a string to a kebab-case. */
+/** Converts a string to kebab-case. */
 export default function toKebabCase( toConvert = '' ) {
 	return toConvert
 		.replace( /[_\W]+(?=\w+)/g, '-' )
