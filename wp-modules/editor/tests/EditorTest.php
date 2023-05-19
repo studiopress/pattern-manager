f2f2cb2e4b48eaf86457e37dd30b65ce4988010c
Phil Johnston <phil.johnston@wpengine.com>
2023-05-19 16:36:25 -0400

    Make editor tests more verbose, and fix array-to-string errors.

--- wp-modules/editor/tests/EditorTest.php
+++ wp-modules/editor/tests/EditorTest.php
@@ -37,13 +37,31 @@ class EditorTest extends WP_UnitTestCase {
 	 */
 	public function test_register_pattern_post_type_meta_types() {
 		register_pattern_post_type();
+		
+		$pattern_default_keys = get_pattern_defaults();
+		$registered_keys      = get_registered_meta_keys( 'post', 'pm_pattern' );
+		
+		// Loop through each default key to make sure its type matches what was actually registered by WP.
+		foreach ( $pattern_default_keys as $meta_key => $default_value ) {
 
-		foreach ( array_diff( get_pattern_defaults(), [ 'title' => null ] ) as $meta_key => $default_value ) {
-			$expected_type = get_registered_meta_keys( 'post', 'pm_pattern' )[ $meta_key ]['type'];
+			// The title and content pattern defaults are not registered meta keys, so skip them.
+			if ( 'title' === $meta_key || 'content' === $meta_key ) {
+				continue;
+			}
+
+			$expected_type = $registered_keys[ $meta_key ]['type'];
+			
+			// Fix the typing for numbers.
+			if ( 'number' === $expected_type ) {
+				$expected_type = 'integer';
+			}
+
+			$actual_type = gettype( $default_value );
+			
 			$this->assertSame(
-				'number' === $expected_type ? 'integer' : $expected_type,
-				gettype( $default_value ),
-				"the type of the default for {$meta_key} is wrong"
+				$expected_type,
+				$actual_type,
+				"The type of the default for {$meta_key} is wrong. It should be {$expected_type} but it was {$actual_type}"
 			);
 		}
 	}
@@ -54,11 +72,23 @@ class EditorTest extends WP_UnitTestCase {
 	public function test_register_pattern_post_type_meta_defaults() {
 		register_pattern_post_type();
 
-		foreach ( array_diff( get_pattern_defaults(), [ 'title' => null ] ) as $meta_key => $default_value ) {
+		$pattern_default_keys = get_pattern_defaults();
+		$registered_keys      = get_registered_meta_keys( 'post', 'pm_pattern' );
+
+		// Loop through each default key to make sure its default value matches what was actually registered by WP.
+		foreach ( $pattern_default_keys as $meta_key => $default_value ) {
+
+			// The title and content pattern defaults are not registered meta keys, so skip them.
+			if ( 'title' === $meta_key || 'content' === $meta_key ) {
+				continue;
+			}
+
+			$actual_value = $registered_keys[ $meta_key ]['default'];
+
 			$this->assertSame(
 				$default_value,
-				get_registered_meta_keys( 'post', 'pm_pattern' )[ $meta_key ]['default'],
-				"the default value of {$meta_key} is wrong"
+				$actual_value,
+				"The value of the default for {$meta_key} is wrong."
 			);
 		}
 	}
