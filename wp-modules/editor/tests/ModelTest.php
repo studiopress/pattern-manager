76559135b9dbd7ed92e300eaa76a5544f2f54c08
Phil Johnston <phil.johnston@wpengine.com>
2023-05-22 16:13:27 -0400

    Add tests for scenarios in question

--- wp-modules/editor/tests/ModelTest.php
+++ wp-modules/editor/tests/ModelTest.php
@@ -83,7 +83,7 @@ class ModelTest extends WP_UnitTestCase {
 	}
 	
 	/**
-	 * Tests delete_pattern_posts.
+	 * Tests that a pattern newly created with Pattern Manager results in a slug that matches the title.
 	 */
 	public function test_new_pattern_title_matches_slug() {
 		
@@ -96,18 +96,90 @@ class ModelTest extends WP_UnitTestCase {
 		$post->post_content   = 'test pattern content';
 		$post->post_status    = 'publish';
 		$post->post_name      = 'new-pattern-originally-created-with-pattern-manager';
-		$post->post_type      = 'page';
+		$post->post_type      = get_pattern_post_type();
 		$post->filter         = 'raw';
 
 		// Convert to WP_Post object.
 		$wp_post = new \WP_Post( $post );
 		
-		// Pass that mocked post into the function we want to test.
-		save_pattern_to_file( $wp_post );
+		// Save the mocked pattern to the disk.
+		\PatternManager\Editor\save_pattern_to_file( $wp_post );
 		
 		// Get the contents of the file that was saved.
-		$pattern = get_pattern_by_name( $post->post_name );
+		$pattern = \PatternManager\PatternDataHandlers\get_pattern_by_name( $post->post_name );
 		
-		print_r( $pattern );
+		// Make sure the slug of the post and the slug in the file match.
+		$this->assertSame($post->post_name, $pattern['slug']);
+		
+		// Make sure the slug of the post and the filename match.
+		$this->assertSame($post->post_name, $pattern['name']);
+
+	}
+	
+	/**
+	 * Tests a pattern with a slug that does not match the filename remains the same if something other than the title was not changed.
+	 */
+	public function test_slug_and_filename_stay_the_same_after_content_update() {
+		
+		// Mock a post object so we can test it.
+		$post_id              = -997; // negative ID, to avoid clash with a valid post.
+		$post                 = new \stdClass();
+		$post->ID             = $post_id;
+		$post->post_author    = 1;
+		$post->post_title     = 'This title remains the same after content changes.';
+		$post->post_content   = 'test pattern content';
+		$post->post_status    = 'publish';
+		$post->post_name      = 'this-slug-remains-the-same-after-content-changes';
+		$post->post_type      = get_pattern_post_type();
+		$post->filter         = 'raw';
+
+		// Convert to WP_Post object.
+		$wp_post = new \WP_Post( $post );
+		
+		// Save the pattern to the disk.
+		\PatternManager\Editor\save_pattern_to_file( $wp_post );
+		
+		// Rename the pattern's filename to something different than the slug.
+		// This is to mock how it might be for for a non-pm-made pattern, with mismatching filenames and slugs.
+		$wp_filesystem = \PatternManager\GetWpFilesystem\get_wp_filesystem_api();
+		$patterns_dir  = \PatternManager\PatternDataHandlers\get_patterns_directory();
+		$original_name = $post->post_name . '.php';
+		$mocked_mismatching_name = 'mismatched-name.php';
+		$wp_filesystem->move( $patterns_dir . $original_name, $patterns_dir . $mocked_mismatching_name );
+		$wp_filesystem->delete( $patterns_dir . $original_name );
+		
+		// Get the contents of the file;
+		$pattern = \PatternManager\PatternDataHandlers\get_pattern_by_name( 'mismatched-name' );
+		
+		// Mock a post object so we can test modifying only the content.
+		$post_id              = -997; // negative ID, to avoid clash with a valid post.
+		$post                 = new \stdClass();
+		$post->ID             = $post_id;
+		$post->post_author    = 1;
+		$post->post_title     = $pattern['title'];
+		$post->post_content   = 'This is modified pattern content, but nothing else is changed!';
+		$post->post_status    = 'publish';
+		$post->post_name      = $pattern['slug'];
+		$post->post_type      = get_pattern_post_type();
+		$post->filter         = 'raw';
+		
+		// Convert to WP_Post object.
+		$wp_post = new \WP_Post( $post );
+
+		// Save the pattern to the disk.
+		\PatternManager\Editor\save_pattern_to_file( $wp_post );
+		
+		// Get the contents of the file;
+		$content_modified_pattern = \PatternManager\PatternDataHandlers\get_pattern_by_name( 'mismatched-name' );
+
+		// Make sure the slug does not get changed when only the content is modified.
+		$this->assertSame('this-slug-remains-the-same-after-content-changes', $content_modified_pattern['slug']);
+		
+		// Make sure the title does not get changed when only the content is modified.
+		$this->assertSame('This title remains the same after content changes.', $content_modified_pattern['title']);
+		
+		// Make sure the filename does not get changed when only the content is modified.
+		$this->assertSame('mismatched-name', $content_modified_pattern['name']);
+
 	}
 }
