f5fcdcff2d752a24592d6fee8cf2b0bf9025eb3f
Ryan Kienstra <kienstraryan@gmail.com>
2023-06-15 15:55:02 -0600

    See if slashing pattern categories helps

---
+++ wp-modules/editor/js/src/utils/stripIllegalChars.ts
@@ -0,0 +1,3 @@
+export default function stripIllegalChars( toConvert: string ) {
+  return toConvert.replace( /[^-\w]/g, '' )
+}
--- wp-modules/editor/js/src/utils/toKebabCase.ts
+++ wp-modules/editor/js/src/utils/toKebabCase.ts
@@ -2,6 +2,5 @@
 export default function toKebabCase( toConvert = '' ) {
 	return toConvert
 		.replace( /[_\W]+(?=\w+)/g, '-' )
-		.replace( /[^-\w]/g, '' )
 		.toLowerCase();
 }
