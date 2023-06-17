31958051c37692a2a2ee878d920f3cbab873acd6
Ryan Kienstra <kienstraryan@gmail.com>
2023-06-16 18:45:54 -0600

    Remove unused toKebabCase, move its tests to the used file (#201)

--- wp-modules/app/js/src/utils/toKebabCase.ts
+++
@@ -1,7 +0,0 @@
-/** Converts a string to kebab-case. */
-export default function toKebabCase( toConvert = '' ) {
-	return toConvert
-		.replace( /[_\W]+(?=\w+)/g, '-' )
-		.replace( /[^-\w]/g, '' )
-		.toLowerCase();
-}
