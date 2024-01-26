0d00b15f736f9aeead1ab0d02bdc5880f863b149
Phil Johnston <phil.johnston@wpengine.com>
2024-01-26 13:53:32 -0500

    Pass patterns array in directly, instead of pre-wrapping it in an array.
    This fixes these linting issues:
    21:5  warning  React Hook useLayoutEffect has a missing dependency: 'dependencies'. Either include it or remove the dependency array                           react-hooks/exhaustive-deps
      21:7  warning  React Hook useLayoutEffect has a complex expression in the dependency array. Extract it to a separate variable so it can be statically checked  react-hooks/exhaustive-deps
    
    To test: Make sure that when you filter which patterns are shown on the page, they are forced to re-render.

--- wp-modules/app/js/src/hooks/useForceRerender.ts
+++ wp-modules/app/js/src/hooks/useForceRerender.ts
@@ -1,16 +1,17 @@
 import { useLayoutEffect, useState } from '@wordpress/element';
+import type { Patterns } from '../types';
 
 type WindowDimensions = [ number, number ];
 
 /** Re-render the calling component when the window is resized or dependencies update. */
 export default function useForceRerender< T extends unknown >(
-	dependencies: T[]
+	dependencies: Patterns
 ) {
 	const [ , setForceUpdate ] = useState< T[] | WindowDimensions >();
 
 	useLayoutEffect( () => {
-		setForceUpdate( dependencies );
-
+		setForceUpdate( [ window.innerWidth, window.innerHeight ] );
+		console.log( 'kjshgdkjhsdg' );
 		function updateSizeAndRerender() {
 			setForceUpdate( [ window.innerWidth, window.innerHeight ] );
 		}
