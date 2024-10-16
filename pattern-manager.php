7f55a31222fff41d527b0156e5c71f36bfc8ddb6
Nick Cernis <nick@cern.is>
2024-10-16 16:45:14 +0200

    chore: bump version to 0.3.2, add changelog

--- pattern-manager.php
+++ pattern-manager.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Pattern Manager
  * Description: Create and maintain patterns.
- * Version: 0.3.1
+ * Version: 0.3.2
  * Author: WP Engine
  * Author URI: wpengine.com
  * Text Domain: pattern-manager
